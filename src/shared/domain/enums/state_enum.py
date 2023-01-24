827d8f6413f5438fd5da4845c7a4d16d1079e97c
TioSundae <85962841+JoaoVitorBranco@users.noreply.github.com>
2023-01-23 22:48:58 -0300

    remove exemplo user

--- src/shared/domain/enums/state_enum.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-
-
-class STATE(Enum):
-    APPROVED = "APPROVED"
-    PENDING = "PENDING"
-    REJECTED = "REJECTED"
--- src/shared/domain/repositories/user_repository_interface.py
+++ src/shared/domain/repositories/user_repository_interface.py
@@ -13,31 +13,4 @@ class IUserRepository(ABC):
         """
         pass
 
-    @abstractmethod
-    def get_all_user(self) -> List[User]:
-        pass
-
-    @abstractmethod
-    def create_user(self, new_user: User) -> User:
-        pass
-
-    @abstractmethod
-    def delete_user(self, user_id: int) -> User:
-        """
-        If user not found raise NoItemsFound
-        """
-        pass
-
-    @abstractmethod
-    def update_user(self, user_id: int, new_name: str) -> User:
-        """
-        If user not found raise NoItemsFound
-        """
-        pass
-
-    @abstractmethod
-    def get_user_counter(self) -> int:
-        """
-        Returns the number of all users that have ever been created
-        """
-        pass
+    
