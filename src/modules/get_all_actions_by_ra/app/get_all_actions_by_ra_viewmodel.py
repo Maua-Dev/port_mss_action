ccc45aae127b410cae88d9600e33778d61b3302f
Luigi Trevisan <checkzin123@gmail.com>
2023-01-29 02:35:52 -0300

    viewmodel + test

---
+++ src/modules/get_all_actions_by_ra/app/get_all_actions_by_ra_viewmodel.py
@@ -0,0 +1,53 @@
+import datetime
+from typing import List
+from src.shared.domain.entities.action import Action
+from src.shared.domain.enums.action_type_enum import ACTION_TYPE
+from src.shared.domain.enums.stack_enum import STACK
+
+class ActionViewModel:
+    owner_ra: str
+    date: datetime.datetime
+    action_id: str
+    title: str
+    duration: datetime.time
+    project_code: str
+    associated_members_ra: List[str] = None
+    stack_tags: List[STACK] = None
+    action_type_tags: List[ACTION_TYPE] = None
+    
+    def __init__(self, action: Action):
+        self.owner_ra = action.owner_ra
+        self.date = action.date
+        self.action_id = action.action_id
+        self.title = action.title
+        self.duration = action.duration
+        self.project_code = action.project_code
+        self.associated_members_ra = action.associated_members_ra
+        self.stack_tags = action.stack_tags
+        self.action_type_tags = action.action_type_tags
+        
+    def to_dict(self):
+        return {
+            "owner_ra": self.owner_ra,
+            "date": self.date.strftime("%d/%m/%Y"),
+            "action_id": self.action_id,
+            "title": self.title,
+            "duration": self.duration.strftime("%H:%M"),
+            "project_code": self.project_code,
+            "associated_members_ra": ', '.join(self.associated_members_ra),
+            "stack_tags": ', '.join([tag.value for tag in self.stack_tags]),
+            "action_type_tags": ', '.join([tag.value for tag in self.action_type_tags])
+        }
+        
+
+class GetAllActionsByRaViewmodel:
+    actions: List[Action]
+    
+    def __init__(self, actions: List[Action]):
+        self.actions = actions
+    
+    def to_dict(self):
+        return {
+            "actions": [ActionViewModel(action).to_dict() for action in self.actions],
+            "message:": "actions retrieved with success"
+        }
---
+++ tests/modules/get_all_actions_by_ra/app/test_get_all_actions_by_ra_viewmodel.py
@@ -0,0 +1,63 @@
+from src.modules.get_all_actions_by_ra.app.get_all_actions_by_ra_viewmodel import GetAllActionsByRaViewmodel
+from src.shared.infra.repositories.action_repository_mock import ActionRepositoryMock
+from src.modules.get_all_actions_by_ra.app.get_all_actions_by_ra_usecase import GetAllActionsByRaUsecase
+
+class Test_GetAllActionsByRaViewmodel:
+    def test_get_all_actions_by_ra_viewmodel(self):
+        repo = ActionRepositoryMock()
+        usecase = GetAllActionsByRaUsecase(repo)
+        actions = usecase(ra='21017310')
+        viewmodel = GetAllActionsByRaViewmodel(actions)
+        expected = {
+            "actions" : [
+                {
+                    'owner_ra' : '21017310',
+                    'date' : '18/10/2021',
+                    'action_id' : 'u1e2',
+                    'associated_members_ra' : '',
+                    'title' : 'Reunião de Diretoria',
+                    'duration' : '02:00',
+                    'project_code' : 'MF',
+                    'stack_tags' : 'INTERNAL',
+                    'action_type_tags' : 'MEETING'
+                },
+                {
+                    'owner_ra' : '21017310',
+                    'date' : '18/10/2021',
+                    'action_id' : 'dd1d',
+                    'associated_members_ra' : '',
+                    'title' : 'Code',
+                    'duration' : '01:00',
+                    'project_code' : 'MF',
+                    'stack_tags' : '',
+                    'action_type_tags' : 'CODE'
+                },
+                {
+                    'owner_ra' : '21010757',
+                    'date' : '24/10/2021',
+                    'action_id' : '9fc2',
+                    'associated_members_ra' : '21017310, 22017310',
+                    'title' : 'Code',
+                    'duration' : '04:30',
+                    'project_code' : 'PT',
+                    'stack_tags' : 'BACKEND, FRONTEND',
+                    'action_type_tags' : 'CODE'
+                },
+                {
+                    'owner_ra' : '21017310',
+                    'date' : '18/10/2022',
+                    'action_id' : 'jf12',
+                    'associated_members_ra' : '',
+                    'title' : 'Reunião',
+                    'duration' : '01:00',
+                    'project_code' : 'MF',
+                    'stack_tags' : 'BACKEND, FRONTEND',
+                    'action_type_tags' : ''
+                }
+            ],
+            "message:": "actions retrieved with success"
+        }
+        
+        assert viewmodel.to_dict() == expected
+        
+        
