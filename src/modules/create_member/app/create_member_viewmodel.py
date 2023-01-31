5d4e6927ba1bc335747595b0b34f675e0416ea00
Luigi Trevisan <checkzin123@gmail.com>
2023-01-30 22:30:35 -0300

    viewmodel fix e update no course enum

--- src/modules/create_member/app/create_member_viewmodel.py
+++ src/modules/create_member/app/create_member_viewmodel.py
@@ -54,7 +54,6 @@ class CreateMemberViewmodel:
         self.projects = member.projects
         
     def to_dict(self) -> dict:
-        course_key = [key for key, value in COURSE.__members__.items() if value == self.course][0]
         return {
             "name" : self.name,
             "email" : self.email,
@@ -63,7 +62,7 @@ class CreateMemberViewmodel:
             "stack" : self.stack.value,
             "year" : self.year,
             "cellphone" : self.cellphone,
-            "course" : course_key,
+            "course" : self.course.value,
             "hired_date" : self.hired_date.isoformat(),
             "deactivated_date" : self.deactivated_date.isoformat() if self.deactivated_date else "",
             "active" : self.active.value,
