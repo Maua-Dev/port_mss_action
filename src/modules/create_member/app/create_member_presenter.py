9a49f645434025c0f3a4a00087b063a16b33d2ca
Luigi Trevisan <checkzin123@gmail.com>
2023-01-30 23:28:19 -0300

    presenter + tests

---
+++ src/modules/create_member/app/create_member_presenter.py
@@ -0,0 +1,15 @@
+from .create_member_controller import CreateMemberController
+from .create_member_usecase import CreateMemberUsecase
+from src.shared.environments import Environments
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+
+repo = Environments.get_action_repo()()
+usecase = CreateMemberUsecase(repo=repo)
+controller = CreateMemberController(usecase=usecase)
+
+def lambda_handler(event, context):
+    httpRequest = LambdaHttpRequest(data=event)
+    response = controller(request=httpRequest)
+    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+    
+    return httpResponse.toDict()
