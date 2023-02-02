e28f6b2e779346a3d62045e3d32ac71a976ef675
Luigi Trevisan <checkzin123@gmail.com>
2023-02-02 18:40:32 -0300

    added start and end time in action entity

--- tests/shared/domain/entities/test_action.py
+++ tests/shared/domain/entities/test_action.py
@@ -1,4 +1,3 @@
-import datetime
 from src.shared.domain.entities.action import Action
 from src.shared.domain.enums.stack_enum import STACK
 from src.shared.domain.enums.action_type_enum import ACTION_TYPE
@@ -11,11 +10,11 @@ class Test_Action:
     def test_action(self):
         action = Action(
             owner_ra="22011020",
-            date=1577847600,
+            start_time=1577847600000,
+            end_time=1577890800000,
             action_id="1234",
             associated_members_ra=["22011021", "22011022"],
             title="Teste",
-            duration=datetime.time(12, 0, 0),
             project_code="TS",
             stack_tags = [STACK.BACKEND],
             action_type_tags = [ACTION_TYPE.CODE]
@@ -23,24 +22,25 @@ class Test_Action:
         
         assert type(action) == Action
         assert action.owner_ra == "22011020"
-        assert action.date == 1577847600
+        assert action.start_time == 1577847600000
+        assert action.end_time == 1577890800000
         assert action.action_id == "1234"
         assert action.associated_members_ra == ["22011021", "22011022"]
         assert action.title == "Teste"
-        assert action.duration == datetime.time(12, 0, 0)
         assert action.project_code == "TS"
         assert action.stack_tags == [STACK.BACKEND]
         assert action.action_type_tags == [ACTION_TYPE.CODE]
+        assert action.end_time - action.start_time == 12 * 60 * 60 * 1000
         
     def test_action_invalid_len_owner_ra(self):
         with pytest.raises(EntityError):
             Action(
                 owner_ra="2201102",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -50,11 +50,11 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra=22011020,
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -64,25 +64,53 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="vitor",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
             )
             
-    def test_action_date_not_datetime(self):
+    def test_action_start_time_not_int(self):
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date="2023-01-24",
+                start_time="2023-01-24",
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
+                project_code="TS",
+                stack_tags = [STACK.BACKEND],
+                action_type_tags = [ACTION_TYPE.CODE]
+            )
+    
+    def test_action_end_time_not_int(self):
+        with pytest.raises(EntityError):
+            Action(
+                owner_ra="22011020",
+                start_time=1577890800000,
+                action_id="1234",
+                associated_members_ra=["22011021", "22011022"],
+                title="Teste",
+                end_time="ontem",
+                project_code="TS",
+                stack_tags = [STACK.BACKEND],
+                action_type_tags = [ACTION_TYPE.CODE]
+            )
+        
+    def test_action_end_time_less_than_start_time(self):
+        with pytest.raises(EntityError):
+            Action(
+                owner_ra="22011020",
+                start_time=1577890800000,
+                action_id="1234",
+                associated_members_ra=["22011021", "22011022"],
+                title="Teste",
+                end_time=1577847600000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -92,11 +120,11 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="123",
                 associated_members_ra=["22011021", "22011022"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -106,11 +134,11 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id=1234,
                 associated_members_ra=["22011021", "22011022"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -120,11 +148,11 @@ class Test_Action:
     def test_action_associated_members_ra_none(self):   
         action = Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=None,
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -135,11 +163,11 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra="22011021",
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -149,11 +177,11 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "2201102"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -163,11 +191,11 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=[22011021, 22011022],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -177,11 +205,11 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011020", "22011021"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -190,11 +218,11 @@ class Test_Action:
     def test_action_associated_members_ra_is_empty_list(self):
         action = Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=[],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -205,11 +233,11 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title=1,
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -219,11 +247,11 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title="A",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -233,38 +261,25 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non neque ligula. Sed tempor eu purus quis fringilla. Donec quis maximus neque, sed ullamcorper neque. Quisque varius, nibh sed laoreet egestas, neque turpis egestas nisi, sit amet gravida sem dui sed lacus. Integer non velit sit amet lacus ultrices sagittis vitae nec justo. In hac habitasse platea dictumst. Nullam eu magna id tellus molestie cursus sit amet et massa. Donec varius pulvinar quam id ullamcorper. Nulla sit amet quam purus. Suspendisse a nulla vel ante finibus interdum. Quisque volutpat accumsan orci a sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nullam eu.",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
             )
-    def test_action_duration_not_time(self):
-        with pytest.raises(EntityError):
-            Action(
-                owner_ra="22011020",
-                date=1577847600,
-                action_id="1234",
-                associated_members_ra=["22011021", "22011022"],
-                title="Teste",
-                duration="12:00:00",
-                project_code="TS",
-                stack_tags = [STACK.BACKEND],
-                action_type_tags = [ACTION_TYPE.CODE]
-            )
-
+            
     def test_action_project_code_not_string(self):
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code=1,
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -274,11 +289,11 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="PORTFOLIO",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -288,11 +303,11 @@ class Test_Action:
     def test_action_stack_tags_none(self):
         action = Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = None,
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -304,11 +319,11 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = STACK.BACKEND,
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -318,11 +333,11 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND, "FRONTEND"],
                 action_type_tags = [ACTION_TYPE.CODE]
@@ -331,11 +346,11 @@ class Test_Action:
     def test_action_action_type_tags_none(self):
         action = Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = None
@@ -347,11 +362,11 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = ACTION_TYPE.CODE
@@ -361,11 +376,11 @@ class Test_Action:
         with pytest.raises(EntityError):
             Action(
                 owner_ra="22011020",
-                date=1577847600,
+                start_time=1577847600000,
                 action_id="1234",
                 associated_members_ra=["22011021", "22011022"],
                 title="Teste",
-                duration=datetime.time(12, 0, 0),
+                end_time=1577890800000,
                 project_code="TS",
                 stack_tags = [STACK.BACKEND],
                 action_type_tags = [ACTION_TYPE.CODE, "REVIEW"]
@@ -373,4 +388,4 @@ class Test_Action:
         
     def test_create_action_duplicated_associated_ra(self):
         with pytest.raises(EntityError):
-            action = Action(owner_ra='17033730', date=1634526000, action_id='82fc', associated_members_ra=['21017310', '21010757', '21010757'], title='Teste', duration=datetime.time(12, 0, 0), project_code='MF', stack_tags=[STACK.BACKEND], action_type_tags=[ACTION_TYPE.CODE])
+            action = Action(owner_ra='17033730', start_time=1634526000, action_id='82fc', associated_members_ra=['21017310', '21010757', '21010757'], title='Teste', end_time=1577890800000, project_code='MF', stack_tags=[STACK.BACKEND], action_type_tags=[ACTION_TYPE.CODE])
