e5ae9ae1bfd1eaefe5d9278f4b6f482dbfb6f4e4
Rubiozito <rafaelrubiocarnes@gmail.com>
2023-05-17 15:47:09 -0300

    testes story_id e associated_members none no viewmodel

--- tests/modules/create_action/app/test_create_action_viewmodel.py
+++ tests/modules/create_action/app/test_create_action_viewmodel.py
@@ -34,29 +34,56 @@ class Test_CreateActionViewmodel:
        
         assert viewmodel == expected
 
-    # def test_create_action_viewmodel_without_tags(self):
-    #     repo = ActionRepositoryMock()
-    #     usecase = CreateActionUsecase(repo=repo)
+    def test_create_action_viewmodel_story_id_is_none(self):
+        repo = ActionRepositoryMock()
+        usecase = CreateActionUsecase(repo=repo)
         
-    #     action = Action(owner_ra='17033730', start_date=1634526000000, duration=2*60*60*1000, action_id='82fc', story_id=100, associated_members_ra=None, title='Teste', end_date=1634536800000, project_code='MF', stack_tags=None, action_type_tag=None)
+        action = Action(owner_ra='17033730', start_date=1634526000000, duration=2*60*60*1000, action_id='82fc', story_id=None, associated_members_ra=['19017310'], title='Teste', end_date=1634536800000, project_code='MF', stack_tags=[STACK.BACKEND], action_type_tag=ACTION_TYPE.CODE)
         
-    #     viewmodel = CreateActionViewmodel(action=usecase(action=action)).to_dict()
+        viewmodel = CreateActionViewmodel(action=usecase(action=action)).to_dict()
         
-    #     expected = {
-    #         'action':{
-    #             'owner_ra':'17033730',
-    #             'start_date':1634526000000,
-    #             'end_date':1634536800000,
-    #             'duration':7200000,
-    #             'action_id':'82fc',
-    #             'story_id': 100,
-    #             'title':'Teste',
-    #             'project_code':'MF',
-    #             'associated_members_ra':[],
-    #             'stack_tags':[],
-    #             'action_type_tag':'CODE'
-    #         },
-    #         'message':'the action was created'
-    #         }
+        expected = {
+            'action':{
+                'owner_ra':'17033730',
+                'start_date':1634526000000,
+                'end_date':1634536800000,
+                'duration':7200000,
+                'action_id':'82fc',
+                'story_id': None,
+                'title':'Teste',
+                'project_code':'MF',
+                'associated_members_ra':['19017310'],
+                'stack_tags':['BACKEND'],
+                'action_type_tag':'CODE'
+            },
+            'message':'the action was created'
+            }
        
-    #     assert viewmodel == expected
+        assert viewmodel == expected
+
+    def test_create_action_viewmodel_associated_members_is_none(self):
+        repo = ActionRepositoryMock()
+        usecase = CreateActionUsecase(repo=repo)
+        
+        action = Action(owner_ra='17033730', start_date=1634526000000, duration=2*60*60*1000, action_id='82fc', story_id=100, associated_members_ra=None, title='Teste', end_date=1634536800000, project_code='MF', stack_tags=[STACK.BACKEND], action_type_tag=ACTION_TYPE.CODE)
+        
+        viewmodel = CreateActionViewmodel(action=usecase(action=action)).to_dict()
+        
+        expected = {
+            'action':{
+                'owner_ra':'17033730',
+                'start_date':1634526000000,
+                'end_date':1634536800000,
+                'duration':7200000,
+                'action_id':'82fc',
+                'story_id': 100,
+                'title':'Teste',
+                'project_code':'MF',
+                'associated_members_ra':[],
+                'stack_tags':['BACKEND'],
+                'action_type_tag':'CODE'
+            },
+            'message':'the action was created'
+            }
+       
+        assert viewmodel == expected
