f3dafe84265b9372e4f7cc1ce0900df5f3beec86
Luigi Trevisan <checkzin123@gmail.com>
2023-07-17 20:53:36 -0300

    controller + TU

---
+++ src/modules/get_project/app/get_project_controller.py
@@ -0,0 +1,45 @@
+from src.modules.get_project.app.get_project_usecase import GetProjectUsecase
+from src.modules.get_project.app.get_project_viewmodel import GetProjectViewmodel
+from src.shared.domain.entities.project import Project
+from src.shared.helpers.errors.controller_errors import MissingParameters, WrongTypeParameter
+from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.helpers.errors.usecase_errors import NoItemsFound
+from src.shared.helpers.external_interfaces.external_interface import IRequest, IResponse
+from src.shared.helpers.external_interfaces.http_codes import OK, BadRequest, InternalServerError, NotFound
+
+
+class GetProjectController:
+    
+    def __init__(self, usecase: GetProjectUsecase):
+        self.usecase = usecase
+        
+    
+    def __call__(self, request: IRequest) -> IResponse:
+        try:
+            if request.data.get('code') is None:
+                raise MissingParameters('code')
+            
+            if type(request.data.get('code')) is not str:
+                raise WrongTypeParameter('code', 'str', type(request.data.get('code')))
+            
+            if not Project.validate_project_code(request.data.get('code')):
+                raise EntityError('code')
+            
+            project = self.usecase(request.data.get('code'))
+            viewmodel = GetProjectViewmodel(project=project[0], members=project[1])
+            return OK(viewmodel.to_dict())
+        
+        except MissingParameters as err:
+            return BadRequest(body=err.message)
+        
+        except NoItemsFound as err:
+            return NotFound(body=err.message)
+
+        except EntityError as err:
+            return BadRequest(body=err.message)
+        
+        except WrongTypeParameter as err:
+            return BadRequest(body=err.message)
+        
+        except Exception as err:
+            return InternalServerError(body=err.args[0])
