b71038f4a208f6ca184a45d8dbfb1a3247710636
Luigi Trevisan <checkzin123@gmail.com>
2023-08-23 19:03:10 -0300

    action repository dynamo + TU

---
+++ src/shared/infra/repositories/action_repository_dynamo.py
@@ -0,0 +1,131 @@
+from typing import List, Optional
+from src.shared.domain.entities.action import Action
+from src.shared.domain.entities.associated_action import AssociatedAction
+from src.shared.domain.entities.member import Member
+from src.shared.domain.entities.project import Project
+from src.shared.domain.repositories.action_repository_interface import IActionRepository
+from src.shared.environments import Environments
+from src.shared.infra.dto.action_dynamo_dto import ActionDynamoDTO
+from src.shared.infra.dto.associated_action_dynamo_dto import AssociatedActionDynamoDTO
+from src.shared.infra.dto.member_dynamo_dto import MemberDynamoDTO
+from src.shared.infra.dto.project_dynamo_dto import ProjectDynamoDTO
+from src.shared.infra.external.dynamo.datasources.dynamo_datasource import DynamoDatasource
+
+class ActionRepositoryDynamo(IActionRepository):
+    @staticmethod
+    def action_partition_key_format(action: Action) -> str:
+        return f'{action.action_id}'
+    
+    @staticmethod
+    def action_sort_key_format(action: Action) -> str:
+        return f'action#{action.action_id}'
+    
+    @staticmethod
+    def project_partition_key_format(project: Project) -> str:
+        return f'project'
+    
+    @staticmethod
+    def project_sort_key_format(project: Project) -> str:
+        return f'project#{project.code}'
+    
+    @staticmethod
+    def member_partition_key_format(member: Member) -> str:
+        return f'member'
+    
+    @staticmethod
+    def member_sort_key_format(member: Member) -> str:
+        return f'member#{member.ra}'
+    
+    @staticmethod
+    def associated_action_partition_key_format(associatedAction: AssociatedAction) -> str:
+        return f'{associatedAction.member_ra}'
+    
+    @staticmethod
+    def associated_action_sort_key_format(associatedAction: AssociatedAction) -> str:
+        return f'associated_action#{associatedAction.action_id}'
+    
+    @staticmethod
+    def gsi1_associated_action_partition_key_format(associatedAction: AssociatedAction) -> str:
+        return f'{associatedAction.action_id}'
+    
+    @staticmethod
+    def gsi1_associated_action_sort_key_format(associatedAction: AssociatedAction) -> str:
+        return f'associated_action#{associatedAction.member_ra}'
+
+    @staticmethod
+    def associated_action_lsi1_partition_key_format(associatedAction: AssociatedAction) -> str:
+        return f'{associatedAction.member_ra}'
+    
+    @staticmethod
+    def associated_action_lsi1_sort_key_format(associatedAction: AssociatedAction) -> str:
+        return f'{associatedAction.start_date}'
+    
+    def __init__(self):
+        self.dynamo = DynamoDatasource(
+            endpoint_url=Environments.get_envs().endpoint_url,
+            dynamo_table_name=Environments.get_envs().dynamo_table_name,
+            region=Environments.get_envs().region,
+            partition_key=Environments.get_envs().dynamo_partition_key,
+            sort_key=Environments.get_envs().dynamo_sort_key,
+        )
+        
+        
+    def create_project(self, project: Project) -> Project:
+        item = ProjectDynamoDTO.from_entity(project).to_dynamo()
+        resp = self.dynamo.put_item(item=item, partition_key=self.project_partition_key_format(project), sort_key=self.project_sort_key_format(project))
+        
+        return project
+    
+    def create_action(self, action: Action) -> Action:
+        item = ActionDynamoDTO.from_entity(action).to_dynamo()
+        resp = self.dynamo.put_item(item=item, partition_key=self.action_partition_key_format(action), sort_key=self.action_sort_key_format(action), is_decimal=True)
+        
+        return action
+    
+    def get_action(self, action_id: str) -> Optional[Action]:
+        pass
+    
+    def create_associated_action(self, associated_action: AssociatedAction) -> AssociatedAction:
+        item = AssociatedActionDynamoDTO.from_entity(associated_action).to_dynamo()
+        resp = self.dynamo.put_item(item=item, partition_key=self.associated_action_partition_key_format(associated_action), sort_key=self.associated_action_sort_key_format(associated_action), is_decimal=True)
+        
+        return associated_action
+            
+    def delete_project(self, code: str) -> Optional[Project]:
+        pass
+    
+    def get_project(self, code: str) -> Project:
+        pass
+
+    def update_project(self, code: str, name: str, description: str, po_RA: str, scrum_RA: str, start_date: int, photos: List[str] = []) -> Project:
+        pass
+    
+    def get_all_projects(self) -> List[Project]:
+        pass
+    
+    def get_all_members(self) -> List[Member]:
+        pass
+
+    def get_member(self, ra: str) -> Member:
+        pass
+    
+    def get_associated_actions_by_ra(self, ra: str, amount: int, start: Optional[int] = None, end: Optional[int] = None, exclusive_start_key: Optional[str] = None) -> List[AssociatedAction]:
+        pass
+    
+    def batch_get_action(self, action_ids: List[str]) -> List[Action]:
+        pass
+    
+    def batch_update_associated_action_start(self, action_id: str, new_start_date: Optional[int] = None, members_ra: Optional[List[str]] = None) -> List[AssociatedAction]:
+        pass
+    
+    def batch_update_associated_action_members(self, action_id: str, members: List[str], start_date: int) -> List[AssociatedAction]:
+        pass
+    
+    def update_action(self, action_id: str, new_owner_ra: Optional[str] = None, new_start_date : Optional[int] = None, new_end_date : Optional[int] = None, new_duration : Optional[int] = None, new_story_id : Optional[str] = None, new_title : Optional[str] = None, new_description : Optional[str] = None, new_project_code : Optional[str] = None, new_associated_members_ra : Optional[List[str]] = None, new_stack_tags : Optional[List[str]] = None, new_action_type_tag : Optional[str] = None) -> Action:
+        pass
+    
+    def create_member(self, member: Member) -> Member:
+        item = MemberDynamoDTO.from_entity(member).to_dynamo()
+        resp = self.dynamo.put_item(item=item, partition_key=self.member_partition_key_format(member), sort_key=self.member_sort_key_format(member), is_decimal=True)
+        
+        return member
---
+++ tests/shared/infra/repositories/test_action_repository_dynamo.py
@@ -0,0 +1,46 @@
+import pytest
+from src.shared.domain.entities.action import Action
+from src.shared.domain.entities.associated_action import AssociatedAction
+from src.shared.domain.entities.member import Member
+from src.shared.domain.entities.project import Project
+from src.shared.domain.enums.action_type_enum import ACTION_TYPE
+from src.shared.domain.enums.active_enum import ACTIVE
+from src.shared.domain.enums.course_enum import COURSE
+from src.shared.domain.enums.role_enum import ROLE
+from src.shared.domain.enums.stack_enum import STACK
+from src.shared.infra.repositories.action_repository_dynamo import ActionRepositoryDynamo
+
+
+class Test_ActionRepositoryDynamo:
+    @pytest.mark.skip("Can't run test in github actions")
+    def test_create_project(self):
+        repo = ActionRepositoryDynamo()
+        project = Project(code="MF",name="Maua Food",description="É um aplicativo #foramoleza",po_RA="21017310",scrum_RA="21010757",start_date=1634576165000,photos=["https://i.imgur.com/gHoRKJU.png"],members=["10017310","21010757","21017310"])
+        resp = repo.create_project(project=project)
+        
+        assert resp == project
+    
+    @pytest.mark.skip("Can't run test in github actions")
+    def test_create_action(self):
+        repo = ActionRepositoryDynamo()
+        action = Action(owner_ra="19017310",action_id="eefe6db8-e03e-42c3-9fd2-1de796139501",story_id=497,associated_members_ra=["23017310","10017310","17033730"],stack_tags=[STACK.INTERNAL],action_type_tag=ACTION_TYPE.ARCHITECT,project_code="SM",title="Retrospective",description="Reunião de planning",start_date=1667256000000,end_date=1690046000000,duration=22790000000)
+        resp = repo.create_action(action=action)
+        
+        assert resp == action
+    
+    @pytest.mark.skip("Can't run test in github actions")
+    def test_create_associated_action(self):
+        repo = ActionRepositoryDynamo()
+        associatedAction = AssociatedAction(member_ra="10017310",action_id="eefe6db8-e03e-42c3-9fd2-1de796139501",start_date=1667256000000)
+        resp = repo.create_associated_action(associated_action=associatedAction)
+        
+        assert resp == associatedAction
+        
+    @pytest.mark.skip("Can't run test in github actions")
+    def test_create_member(self):
+        repo = ActionRepositoryDynamo()
+        member = Member(name="Joao Branco",email_dev="jbranco.devmaua@gmail.com",email="jbranco@gmail.com",ra="21010757",role=ROLE.HEAD,stack=STACK.BACKEND,year=3,cellphone="11991152348",course=COURSE.ECM,hired_date=1634921765000,active=ACTIVE.ACTIVE)
+        
+        resp = repo.create_member(member=member)
+        
+        assert resp == member
