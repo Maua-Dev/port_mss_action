64ea72bd7c50633018a2f9a9a644d8e5bcdb6155
Rubiozito <rafaelrubiocarnes@gmail.com>
2023-09-04 11:53:00 -0300

    dynamo_stack + iac_stack

--- iac/iac/iac_stack.py
+++ iac/iac/iac_stack.py
@@ -5,7 +5,7 @@ from aws_cdk import (
 )
 from constructs import Construct
 
-# from .dynamo_stack import DynamoStack
+from .dynamo_stack import DynamoStack
 from .lambda_stack import LambdaStack
 from aws_cdk.aws_apigateway import RestApi, Cors
 
@@ -37,14 +37,19 @@ class IacStack(Stack):
 
         ENVIRONMENT_VARIABLES = {
             "STAGE": "TEST",
-            # "DYNAMO_TABLE_NAME": self.dynamo_stack.dynamo_table.table_name,
-            # "DYNAMO_PARTITION_KEY": self.dynamo_stack.partition_key_name,
-            # "DYNAMO_SORT_KEY": self.dynamo_stack.sort_key_name,
+            "DYNAMO_TABLE_NAME": self.dynamo_stack.dynamo_table.table_name,
+            "DYNAMO_PARTITION_KEY": self.dynamo_stack.partition_key_name,
+            "DYNAMO_SORT_KEY": self.dynamo_stack.sort_key_name,
+            "DYNAMO_GSI_PARTITION_KEY": "GSI1-PK",
+            "DYNAMO_GSI_SORT_KEY": "GSI1-SK",
+            "DYNAMO_LSI_SORT_KEY": "LSI1-SK",
             "REGION": self.region,
         }
 
         self.lambda_stack = LambdaStack(self, api_gateway_resource=api_gateway_resource,
                                         environment_variables=ENVIRONMENT_VARIABLES)
 
-        # self.dynamo_stack = DynamoStack(self)
+        self.dynamo_stack = DynamoStack(self)
+
+
 
