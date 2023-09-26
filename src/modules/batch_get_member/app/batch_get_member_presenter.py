2bebadf74cbd8b2ddcbc504abed134407ec13c0a
Luigi Trevisan <checkzin123@gmail.com>
2023-09-26 14:28:31 -0300

    presenter + TU

---
+++ src/modules/batch_get_member/app/batch_get_member_presenter.py
@@ -0,0 +1,15 @@
+from .batch_get_member_usecase import BatchGetMemberUsecase
+from .batch_get_member_controller import BatchGetMemberController
+from src.shared.environments import Environments
+from src.shared.helpers.external_interfaces.http_lambda_requests import LambdaHttpRequest, LambdaHttpResponse
+
+repo = Environments.get_action_repo()()
+usecase = BatchGetMemberUsecase(repo=repo)
+controller = BatchGetMemberController(usecase=usecase)
+
+def lambda_handler(event, context):
+    httpRequest = LambdaHttpRequest(data=event)
+    response = controller(httpRequest)
+    httpResponse = LambdaHttpResponse(status_code=response.status_code, body=response.body, headers=response.headers)
+
+    return httpResponse.toDict()
