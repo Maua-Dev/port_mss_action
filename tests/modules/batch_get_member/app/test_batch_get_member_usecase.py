5f0d6648d9c5aa9fe972a7c57b8e718b1bb7661d
Luigi Trevisan <checkzin123@gmail.com>
2023-09-26 14:05:37 -0300

    batch get member usecase

---
+++ tests/modules/batch_get_member/app/test_batch_get_member_usecase.py
@@ -0,0 +1,24 @@
+from src.modules.batch_get_member.app.batch_get_member_usecase import BatchGetMemberUsecase
+from src.shared.domain.entities.member import Member
+from src.shared.infra.repositories.action_repository_mock import ActionRepositoryMock
+
+
+class Test_BatchGetMemberUsecase:
+    def test_batch_get_member_usecase(self):
+        repo = ActionRepositoryMock()
+        usecase = BatchGetMemberUsecase(repo=repo)
+        
+        members = usecase(ras=[repo.members[0].ra, repo.members[1].ra])
+        assert len(members) == 2
+        assert all([type(member) == Member for member in members])
+        assert members[0] == repo.members[0]
+        assert members[1] == repo.members[1]
+        
+    def test_batch_get_member_usecase_with_duplicates(self):
+        repo = ActionRepositoryMock()
+        usecase = BatchGetMemberUsecase(repo=repo)
+        
+        members = usecase(ras=[repo.members[0].ra, repo.members[0].ra])
+        assert len(members) == 1
+        assert all([type(member) == Member for member in members])
+        assert members[0] == repo.members[0]
