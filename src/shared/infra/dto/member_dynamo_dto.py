a5eac8e98667f9294fa3d74caead36721b925294
GabrielBianconiconi <gabrielbianconiconi@gmail.com>
2023-10-19 17:45:34 -0300

    added user_id to member_dynamo_dto ,create not done

--- src/shared/infra/dto/member_dynamo_dto.py
+++ src/shared/infra/dto/member_dynamo_dto.py
@@ -20,8 +20,9 @@ class MemberDynamoDTO:
     hired_date: int
     deactivated_date: Optional[int] = None
     active: ACTIVE
+    user_id: str
     
-    def __init__(self, name: str, email_dev: str, email: str, ra: str, role: ROLE, stack: STACK, year: int, cellphone: str, course: COURSE, hired_date: int, active: ACTIVE, deactivated_date: Optional[int] = None):
+    def __init__(self, name: str, email_dev: str, email: str, ra: str, role: ROLE, stack: STACK, year: int, cellphone: str, course: COURSE, hired_date: int, active: ACTIVE, user_id: str, deactivated_date: Optional[int] = None):
         self.name = name
         self.email_dev = email_dev
         self.email = email
@@ -34,6 +35,7 @@ class MemberDynamoDTO:
         self.hired_date = hired_date
         self.deactivated_date = deactivated_date
         self.active = active
+        self.user_id = user_id
         
     @staticmethod
     def from_entity(member: Member) -> "MemberDynamoDTO":
@@ -49,7 +51,8 @@ class MemberDynamoDTO:
             course=member.course,
             hired_date=member.hired_date,
             deactivated_date=member.deactivated_date,
-            active=member.active
+            active=member.active,
+            user_id=member.user_id
         )
         
     def to_dynamo(self) -> dict:
@@ -66,7 +69,8 @@ class MemberDynamoDTO:
             "course": self.course.value,
             "hired_date": Decimal(self.hired_date),
             "deactivated_date": Decimal(self.deactivated_date) if self.deactivated_date else None,
-            "active": self.active.value
+            "active": self.active.value,
+            "user_id": self.user_id
         }
         
         data_without_none_values = {k: v for k, v in data.items() if v is not None}
@@ -86,7 +90,8 @@ class MemberDynamoDTO:
             course=COURSE(member_data["course"]),
             hired_date=int(member_data["hired_date"]),
             deactivated_date=int(member_data["deactivated_date"]) if "deactivated_date" in member_data else None,
-            active=ACTIVE(member_data["active"])
+            active=ACTIVE(member_data["active"]),
+            user_id=member_data["user_id"]
         )
     
     def to_entity(self) -> Member:   
@@ -102,13 +107,14 @@ class MemberDynamoDTO:
             course=self.course,
             hired_date=self.hired_date,
             deactivated_date=self.deactivated_date,
-            active=self.active
+            active=self.active,
+            user_id=self.user_id
         )
         
     def __repr__(self): 
-        return f"MemberDynamoDTO(name={self.name}, email_dev={self.email_dev}, email={self.email}, ra={self.ra}, role={self.role}, stack={self.stack}, year={self.year}, cellphone={self.cellphone}, course={self.course}, hired_date={self.hired_date}, deactivated_date={self.deactivated_date}, active={self.active})"
+        return f"MemberDynamoDTO(name={self.name}, email_dev={self.email_dev}, email={self.email}, ra={self.ra}, role={self.role}, stack={self.stack}, year={self.year}, cellphone={self.cellphone}, course={self.course}, hired_date={self.hired_date}, deactivated_date={self.deactivated_date}, active={self.active}, user_id={self.user_id})"
     
     def __eq__(self, other):
         if not isinstance(other, MemberDynamoDTO):
             return False
-        return self.name == other.name and self.email_dev == other.email_dev and self.email == other.email and self.ra == other.ra and self.role == other.role and self.stack == other.stack and self.year == other.year and self.cellphone == other.cellphone and self.course == other.course and self.hired_date == other.hired_date and self.deactivated_date == other.deactivated_date and self.active == other.active
+        return self.name == other.name and self.email_dev == other.email_dev and self.email == other.email and self.ra == other.ra and self.role == other.role and self.stack == other.stack and self.year == other.year and self.cellphone == other.cellphone and self.course == other.course and self.hired_date == other.hired_date and self.deactivated_date == other.deactivated_date and self.active == other.active and self.user_id == other.user_id
