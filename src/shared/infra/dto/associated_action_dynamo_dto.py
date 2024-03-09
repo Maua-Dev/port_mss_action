19686ad47b0f1ede5b3c8aebf3fad89944117e70
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-03-09 10:31:39 -0300

    Adiciona campo user_id na classe AssociatedActionDynamoDTO

--- src/shared/infra/dto/associated_action_dynamo_dto.py
+++ src/shared/infra/dto/associated_action_dynamo_dto.py
@@ -7,18 +7,21 @@ class AssociatedActionDynamoDTO:
     member_ra: str
     action_id: str
     start_date: int
+    user_id: str
     
-    def __init__(self, member_ra: str, action_id: str, start_date: int):
+    def __init__(self, member_ra: str, action_id: str, start_date: int, user_id: str):
         self.member_ra = member_ra
         self.action_id = action_id
         self.start_date = start_date
+        self.user_id = user_id
         
     @staticmethod
     def from_entity(self) -> "AssociatedActionDynamoDTO":
         return AssociatedActionDynamoDTO(
             member_ra=self.member_ra,
             action_id=self.action_id,
-            start_date=self.start_date
+            start_date=self.start_date,
+            user_id=self.user_id
         )
         
     def to_dynamo(self) -> dict:
@@ -26,7 +29,8 @@ class AssociatedActionDynamoDTO:
             "entity": "associated_action",
             "member_ra": self.member_ra,
             "action_id": self.action_id,
-            "start_date": Decimal(str(self.start_date))
+            "start_date": Decimal(str(self.start_date)),
+            "user_id": self.user_id
         }
         
     @staticmethod
@@ -34,20 +38,22 @@ class AssociatedActionDynamoDTO:
         return AssociatedActionDynamoDTO(
             member_ra=data['member_ra'],
             action_id=data['action_id'],
-            start_date=int(data['start_date'])
+            start_date=int(data['start_date']),
+            user_id=data['user_id']
         )
         
     def to_entity(self) -> AssociatedAction:
         return AssociatedAction(
             member_ra=self.member_ra,
             action_id=self.action_id,
-            start_date=self.start_date
+            start_date=self.start_date,
+            user_id=self.user_id
         )
         
     def __repr__(self):
-        return f"AssociatedActionDynamoDTO(member_ra={self.member_ra}, action_id={self.action_id}, start_date={self.start_date}"
+        return f"AssociatedActionDynamoDTO(member_ra={self.member_ra}, action_id={self.action_id}, start_date={self.start_date}, user_id={self.user_id})"
     
     def __eq__(self, other):
         if not isinstance(other, AssociatedActionDynamoDTO):
             return False
-        return self.member_ra == other.member_ra and self.action_id == other.action_id and self.start_date == other.start_date
+        return self.member_ra == other.member_ra and self.action_id == other.action_id and self.start_date == other.start_date and self.user_id == other.user_id
--- tests/shared/infra/dto/test_associated_action_dynamo_dto.py
+++ tests/shared/infra/dto/test_associated_action_dynamo_dto.py
@@ -3,25 +3,25 @@ from src.shared.infra.dto.associated_action_dynamo_dto import AssociatedActionDy
 
 class Test_AssociatedActionDynamoDTO:
     def test_associated_action_dynamo_dto_from_entity(self):
-        associated_action = AssociatedAction(member_ra='10017310',action_id='5f4f13df-e7d3-4a10-9219-197ceae9e3f0',start_date=1644256000000)
+        associated_action = AssociatedAction(member_ra='10017310',action_id='5f4f13df-e7d3-4a10-9219-197ceae9e3f0',start_date=1644256000000, user_id='6f5g4h7J-876j-0098-123hb-hgb567fy4hb')
         associated_action_dto = AssociatedActionDynamoDTO.from_entity(associated_action)
         
-        assert associated_action_dto == AssociatedActionDynamoDTO(member_ra='10017310',action_id='5f4f13df-e7d3-4a10-9219-197ceae9e3f0',start_date=1644256000000)
+        assert associated_action_dto == AssociatedActionDynamoDTO(member_ra='10017310',action_id='5f4f13df-e7d3-4a10-9219-197ceae9e3f0',start_date=1644256000000, user_id='6f5g4h7J-876j-0098-123hb-hgb567fy4hb')
         
     def test_associated_action_dynamo_dto_to_dynamo(self):
-        associated_action_dto = AssociatedActionDynamoDTO(member_ra='10017310',action_id='5f4f13df-e7d3-4a10-9219-197ceae9e3f0',start_date=1644256000000)
+        associated_action_dto = AssociatedActionDynamoDTO(member_ra='10017310',action_id='5f4f13df-e7d3-4a10-9219-197ceae9e3f0',start_date=1644256000000, user_id='6f5g4h7J-876j-0098-123hb-hgb567fy4hb')
         associated_action_dynamo = associated_action_dto.to_dynamo()
         
-        assert associated_action_dynamo == {'entity': 'associated_action', 'member_ra': '10017310', 'action_id': '5f4f13df-e7d3-4a10-9219-197ceae9e3f0', 'start_date': 1644256000000}
+        assert associated_action_dynamo == {'entity': 'associated_action', 'member_ra': '10017310', 'action_id': '5f4f13df-e7d3-4a10-9219-197ceae9e3f0', 'start_date': 1644256000000, 'user_id': '6f5g4h7J-876j-0098-123hb-hgb567fy4hb'}
         
     def test_associated_action_dynamo_dto_from_dynamo(self):
-        associated_action_dynamo = {'entity': 'associated_action', 'member_ra': '10017310', 'action_id': '5f4f13df-e7d3-4a10-9219-197ceae9e3f0', 'start_date': 1644256000000}
+        associated_action_dynamo = {'entity': 'associated_action', 'member_ra': '10017310', 'action_id': '5f4f13df-e7d3-4a10-9219-197ceae9e3f0', 'start_date': 1644256000000, 'user_id': '6f5g4h7J-876j-0098-123hb-hgb567fy4hb'}
         associated_action_dto = AssociatedActionDynamoDTO.from_dynamo(associated_action_dynamo)
         
-        assert associated_action_dto == AssociatedActionDynamoDTO(member_ra='10017310',action_id='5f4f13df-e7d3-4a10-9219-197ceae9e3f0',start_date=1644256000000)
+        assert associated_action_dto == AssociatedActionDynamoDTO(member_ra='10017310',action_id='5f4f13df-e7d3-4a10-9219-197ceae9e3f0',start_date=1644256000000, user_id='6f5g4h7J-876j-0098-123hb-hgb567fy4hb')
         
     def test_associated_action_dynamo_dto_to_entity(self):
-        associated_action_dto = AssociatedActionDynamoDTO(member_ra='10017310',action_id='5f4f13df-e7d3-4a10-9219-197ceae9e3f0',start_date=1644256000000)
+        associated_action_dto = AssociatedActionDynamoDTO(member_ra='10017310',action_id='5f4f13df-e7d3-4a10-9219-197ceae9e3f0',start_date=1644256000000, user_id='6f5g4h7J-876j-0098-123hb-hgb567fy4hb')
         associated_action = associated_action_dto.to_entity()
         
-        assert associated_action == AssociatedAction(member_ra='10017310',action_id='5f4f13df-e7d3-4a10-9219-197ceae9e3f0',start_date=1644256000000)
+        assert associated_action == AssociatedAction(member_ra='10017310',action_id='5f4f13df-e7d3-4a10-9219-197ceae9e3f0',start_date=1644256000000, user_id='6f5g4h7J-876j-0098-123hb-hgb567fy4hb')
