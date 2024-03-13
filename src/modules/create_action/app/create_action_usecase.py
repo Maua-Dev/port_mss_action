db4fdea5e1681826caecad0bf625387cd913c362
MateusCMartins724 <mateus.c.martins@outlook.com>
2024-03-12 22:10:40 -0300

    add is_valid to create action usecase

--- src/modules/create_action/app/create_action_usecase.py
+++ src/modules/create_action/app/create_action_usecase.py
@@ -11,7 +11,7 @@ class CreateActionUsecase:
     def __init__(self, repo: IActionRepository):
         self.repo = repo
         
-    def __call__(self, owner_ra: str, start_date: int, stack_tags: List[STACK], end_date: int, duration: int, title: str, project_code: str, action_type_tag: ACTION_TYPE, associated_members_ra: List[str] = [], description: Optional[str] = None, story_id: Optional[int] = None) -> Action:
+    def __call__(self, owner_ra: str, user_id: str, is_valid: bool, start_date: int, stack_tags: List[STACK], end_date: int, duration: int, title: str, project_code: str, action_type_tag: ACTION_TYPE, associated_members_ra: List[str] = [], description: Optional[str] = None, story_id: Optional[int] = None) -> Action:
         
         action_id = str(uuid.uuid4())
         
@@ -19,10 +19,10 @@ class CreateActionUsecase:
             if not self.repo.get_member(ra):
                 raise NoItemsFound(ra)
         
-        action = Action(owner_ra, start_date, stack_tags, end_date, duration, action_id, title, project_code, action_type_tag, associated_members_ra, description, story_id)        
+        action = Action(owner_ra, user_id, start_date, stack_tags, end_date, duration, action_id, is_valid, title, project_code, action_type_tag, associated_members_ra, description, story_id)        
         
         self.repo.create_action(action)
-        self.repo.create_associated_action(AssociatedAction(owner_ra, action_id, start_date))
+        self.repo.create_associated_action(AssociatedAction(owner_ra, action_id, start_date, user_id))
         if action.associated_members_ra:
             for ra in action.associated_members_ra:
                 associated_action = AssociatedAction(ra, action_id, start_date)
