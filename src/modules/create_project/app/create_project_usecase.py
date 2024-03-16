5bfab9e98b1df294e8ce56fbb03b4347365a3e33
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-03-16 08:51:18 -0300

    Create project usecase com user_id e sem RA

--- src/modules/create_project/app/create_project_usecase.py
+++ src/modules/create_project/app/create_project_usecase.py
@@ -1,17 +1,14 @@
 from typing import List
 from src.shared.domain.entities.project import Project
 from src.shared.domain.repositories.action_repository_interface import IActionRepository
-from src.shared.helpers.errors.domain_errors import EntityError
+from src.shared.helpers.errors.usecase_errors import DuplicatedItem
 
 
 class CreateProjectUsecase:
     def __init__(self, repo: IActionRepository):
         self.repo = repo
         
-    def __call__(self, code: str, name: str, description: str, po_user_id: str, scrum_user_id: str, start_date: int, members: List[str], photos: list = None) -> Project:
-        
-        # if self.repo.get_project(project_id=project.project_id) is not None:
-        #     raise DuplicatedItem('project_id')
+    def __call__(self, code: str, name: str, description: str, po_user_id: str, scrum_user_id: str, start_date: int, members_user_ids: List[str], photos: list = None) -> Project:
         
         project = Project(
             code=code,
@@ -21,8 +18,11 @@ class CreateProjectUsecase:
             scrum_user_id=scrum_user_id,
             start_date=start_date,
             photos=photos,
-            members=members
+            members_user_ids=members_user_ids
         )
+
+        if self.repo.get_project(code=project.code) is not None:
+            raise DuplicatedItem('code')
         
         return self.repo.create_project(project)
         
