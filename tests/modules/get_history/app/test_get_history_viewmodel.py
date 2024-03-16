1a42568f310d50daf81c327254be4e1faa7d17da
MateusCMartins724 <mateus.c.martins@outlook.com>
2024-03-16 13:35:36 -0300

    add user_id to get_history_viewmodel

--- tests/modules/get_history/app/test_get_history_viewmodel.py
+++ tests/modules/get_history/app/test_get_history_viewmodel.py
@@ -7,14 +7,13 @@ class Test_GetHistoryViewmodel:
     def test_get_history_viewmodel(self):
         repo = ActionRepositoryMock()
         usecase = GetHistoryUsecase(repo)
-        actions, last_evaluated_key = usecase(ra='17033730')
+        actions, last_evaluated_key = usecase(user_id='7465hvnb-143g-1675-86HnG-75hgnFbcg36')
         viewmodel = GetHistoryViewmodel(
             actions=actions, last_evaluated_key=last_evaluated_key).to_dict()
 
         expected = {
             'actions': [
                 {
-                    'owner_ra': '19017310',
                     'start_date': 1688646000000,
                     'end_date': 1689966000000,
                     'duration': 1320000000,
@@ -23,20 +22,17 @@ class Test_GetHistoryViewmodel:
                     'title': 'Revisão',
                     'description': 'Sprint Planning',
                     'project_code': 'GM',
-                    'associated_members_ra': [
-                        '10017310',
-                        '23017310',
-                        '19017311',
-                        '17033730',
-                        '22017310'
+                    'associated_members_user_ids': [
+                        '93bc6ada-c0d1-7054-66ab-e17414c48ae3',
+                        '75648hbr-184n-1985-91han-7ghn4HgF182',
+                        '7gh5yf5H-857H-1234-75hng-94832hvng1s',
+                        '7465hvnb-143g-1675-86HnG-75hgnFbcg36',
+                        '76h35dg4-h76v-1875-987hn-h67gfv45Gt4'
                     ],
-                    'stack_tags':[
-                        'BACKEND'
-                    ],
-                    'action_type_tag':'WORK'
+                    'stack_tags': ['BACKEND'],
+                    'action_type_tag': 'WORK'
                 },
                 {
-                    'owner_ra': '19017310',
                     'start_date': 1667256000000,
                     'end_date': 1690046000000,
                     'duration': 22790000000,
@@ -45,18 +41,15 @@ class Test_GetHistoryViewmodel:
                     'title': 'Retrospective',
                     'description': 'Reunião de planning',
                     'project_code': 'SM',
-                    'associated_members_ra': [
-                        '23017310',
-                        '10017310',
-                        '17033730'
+                    'associated_members_user_ids': [
+                        '93bc6ada-c0d1-7054-66ab-e17414c48ae3',
+                        '75648hbr-184n-1985-91han-7ghn4HgF182',
+                        '6f5g4h7J-876j-0098-123hb-hgb567fy4hb'
                     ],
-                    'stack_tags':[
-                        'INTERNAL'
-                    ],
-                    'action_type_tag':'ARCHITECT'
+                    'stack_tags': ['INTERNAL'],
+                    'action_type_tag': 'ARCHITECT'
                 },
                 {
-                    'owner_ra': '10017310',
                     'start_date': 1663116000000,
                     'end_date': 1683606000000,
                     'duration': 20490000000,
@@ -65,19 +58,16 @@ class Test_GetHistoryViewmodel:
                     'title': 'Desenvolvimento',
                     'description': 'Revisão de sprint',
                     'project_code': 'MF',
-                    'associated_members_ra': [
-                        '21010757',
-                        '17033730',
-                        '19017310',
-                        '23017310'
+                    'associated_members_user_ids': [
+                        '6574hgyt-785n-9134-18gn4-7gh5uvn36cG',
+                        '75648hbr-184n-1985-91han-7ghn4HgF182',
+                        '7465hvnb-143g-1675-86HnG-75hgnFbcg36',
+                        '51ah5jaj-c9jm-1345-666ab-e12341c14a3'
                     ],
-                    'stack_tags':[
-                        'INFRA'
-                    ],
-                    'action_type_tag':'LEARN'
+                    'stack_tags': ['INFRA'],
+                    'action_type_tag': 'LEARN'
                 },
                 {
-                    'owner_ra': '19017311',
                     'start_date': 1658136000000,
                     'end_date': 1678116000000,
                     'duration': 19980000000,
@@ -86,20 +76,17 @@ class Test_GetHistoryViewmodel:
                     'title': 'Retrospective',
                     'description': 'Reunião de planning',
                     'project_code': 'SF',
-                    'associated_members_ra': [
-                        '17033730',
-                        '22017310',
-                        '10017310',
-                        '23017310',
-                        '21017310'
+                    'associated_members_user_ids': [
+                        '7465hvnb-143g-1675-86HnG-75hgnFbcg36',
+                        '6574hgyt-785n-9134-18gn4-7gh5uvn36cG',
+                        '76h35dg4-h76v-1875-987hn-h67gfv45Gt4',
+                        '6f5g4h7J-876j-0098-123hb-hgb567fy4hb',
+                        '75648hbr-184n-1985-91han-7ghn4HgF182'
                     ],
-                    'stack_tags':[
-                        'FRONTEND'
-                    ],
-                    'action_type_tag':'DESIGN'
+                    'stack_tags': ['FRONTEND'],
+                    'action_type_tag': 'DESIGN'
                 },
                 {
-                    'owner_ra': '10017310',
                     'start_date': 1641896000000,
                     'end_date': 1679686000000,
                     'duration': 37790000000,
@@ -108,20 +95,21 @@ class Test_GetHistoryViewmodel:
                     'title': 'Retrospectiva',
                     'description': 'Revisão de sprint',
                     'project_code': 'SF',
-                    'associated_members_ra': [
-                        '21017310',
-                        '19017310',
-                        '19017311',
-                        '23017310',
-                        '17033730'
+                    'associated_members_user_ids': [
+                        '6574hgyt-785n-9134-18gn4-7gh5uvn36cG',
+                        '7gh5yf5H-857H-1234-75hng-94832hvng1s',
+                        '75648hbr-184n-1985-91han-7ghn4HgF182',
+                        '93bc6ada-c0d1-7054-66ab-e17414c48ae3',
+                        '7465hvnb-143g-1675-86HnG-75hgnFbcg36'
                     ],
-                    'stack_tags':[
-                        'INFRA'
-                    ],
-                    'action_type_tag':'WORK'
+                    'stack_tags': ['INFRA'],
+                    'action_type_tag': 'WORK'
                 }
             ],
-            'last_evaluated_key': {'action_id' : '42e01f11-283c-4925-b0aa-e80ac6c1815a', 'start_date' : 1641896000000},
+            'last_evaluated_key': {
+                'action_id': '42e01f11-283c-4925-b0aa-e80ac6c1815a',
+                'start_date': 1641896000000
+            },
             'message': 'the history was retrieved'
         }
 
@@ -130,7 +118,7 @@ class Test_GetHistoryViewmodel:
     def test_get_history_viewmodel_with_no_actions(self):
         repo = ActionRepositoryMock()
         usecase = GetHistoryUsecase(repo)
-        actions, last_evaluated_key = usecase(ra='21310757')
+        actions, last_evaluated_key = usecase(user_id='6fag4h7J-876j-0098-123hb-hgb567fy4hb')
         viewmodel = GetHistoryViewmodel(
             actions=actions, last_evaluated_key=last_evaluated_key).to_dict()
 
