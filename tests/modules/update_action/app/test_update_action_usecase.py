5167f2c0b061c2e622cee232d20512287069f71a
MateusCMartins724 <mateus.c.martins@outlook.com>
2024-03-16 15:42:33 -0300

    change update action usecase

--- tests/modules/update_action/app/test_update_action_usecase.py
+++ tests/modules/update_action/app/test_update_action_usecase.py
@@ -11,10 +11,10 @@ class Test_UpdateActionUsecase:
         repo = ActionRepositoryMock()
         usecase = UpdateActionUsecase(repo=repo)
 
-        action = usecase(action_id=repo.actions[0].action_id, new_owner_ra='23017310', new_start_date=1634526000000, new_story_id=100, new_associated_members_ra=['19017311'], new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE)
+        action = usecase(action_id=repo.actions[0].action_id, new_user_id='51ah5jaj-c9jm-1345-666ab-e12341c14a3', new_start_date=1634526000000, new_story_id=100, new_associated_members_user_ids=['51ah5jaj-c9jm-1345-666ab-e12341c14a3'], new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE)
         
         assert repo.actions[0] == action
-        assert all(action.member_ra in ['23017310', '19017311'] for action in repo.associated_actions if action.action_id == repo.actions[0].action_id)
+        assert all(action.user_id in ['51ah5jaj-c9jm-1345-666ab-e12341c14a3'] for action in repo.associated_actions if action.action_id == repo.actions[0].action_id)
         assert all(action.start_date == 1634526000000 for action in repo.associated_actions if action.action_id == repo.actions[0].action_id)
         
     def test_update_action_no_items_found(self):
@@ -22,4 +22,4 @@ class Test_UpdateActionUsecase:
         usecase = UpdateActionUsecase(repo=repo)
 
         with pytest.raises(NoItemsFound):
-            action = usecase(action_id='01538473-35ea-43c5-a823-f278c3fb42f3', new_owner_ra='23017310', new_start_date=1634526000000, new_story_id=100, new_associated_members_ra=['19017311'], new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE)
+            action = usecase(action_id='01538473-35ea-43c5-a823-f278c3fb42f3', new_user_id='51ah5jaj-c9jm-1345-666ab-e12341c14a3', new_start_date=1634526000000, new_story_id=100, new_associated_members_user_ids=['19017311'], new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE)
