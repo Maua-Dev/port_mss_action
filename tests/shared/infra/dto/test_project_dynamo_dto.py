fc79177d3dedcaa054221d105857a5e1e294a8e3
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-03-16 00:28:58 -0300

    testes project DTO( 2 com erro)

--- tests/shared/infra/dto/test_project_dynamo_dto.py
+++ tests/shared/infra/dto/test_project_dynamo_dto.py
@@ -9,34 +9,33 @@ class Test_ProjectDynamoDTO:
             code="PT",
             name="Portfólio",
             description="É um site",
-            po_user_id="22011020",
-            scrum_user_id="21010757",
+            po_user_id="51ah5jaj-c9jm-1345-666ab-e12341c14a3",
+            scrum_user_id="6f5g4h7J-876j-0098-123hb-hgb567fy4hb",
             start_date=1673535600000,
             photos=["https://i.imgur.com/gHoRKJU.png"],
-            members=["10017310", "21010757", "22011020"],
-            members_user_ids=["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ]
+            members_user_ids=["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3"]
         )
 
         project_dto = ProjectDynamoDTO.from_entity(project)
         assert project_dto.members_user_ids == ["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ]
-        assert project_dto == ProjectDynamoDTO(code="PT", name="Portfólio", description="É um site", po_user_id="22011020", scrum_user_id="21010757", start_date=1673535600000, photos=["https://i.imgur.com/gHoRKJU.png"], members=["10017310", "21010757", "22011020"], members_user_ids=["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ])
+        assert project_dto == ProjectDynamoDTO(code="PT", name="Portfólio", description="É um site", po_user_id="51ah5jaj-c9jm-1345-666ab-e12341c14a3", scrum_user_id="6f5g4h7J-876j-0098-123hb-hgb567fy4hb", start_date=1673535600000, photos=["https://i.imgur.com/gHoRKJU.png"], members_user_ids=['6f5g4h7J-876j-0098-123hb-hgb567fy4hb', '51ah5jaj-c9jm-1345-666ab-e12341c14a3', '93bc6ada-c0d1-7054-66ab-e17414c48ae3'])
         
     def test_project_dynamo_dto_to_dynamo(self):       
-        project_dto = ProjectDynamoDTO(code="PT", name="Portfólio", description="É um site", po_user_id="22011020", scrum_user_id="21010757", start_date=1673535600000, photos=["https://i.imgur.com/gHoRKJU.png"], members=["10017310", "21010757", "22011020"], members_user_ids=["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ])
+        project_dto = ProjectDynamoDTO(code="PT", name="Portfólio", description="É um site", po_user_id="51ah5jaj-c9jm-1345-666ab-e12341c14a3", scrum_user_id="6f5g4h7J-876j-0098-123hb-hgb567fy4hb", start_date=1673535600000, photos=["https://i.imgur.com/gHoRKJU.png"], members_user_ids=["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ])
         
         project_dynamo = project_dto.to_dynamo()
         
-        assert project_dynamo == {"entity": "project", "code": "PT", "name": "Portfólio", "description": "É um site", "po_user_id": "22011020", "scrum_user_id": "21010757", "start_date": 1673535600000, "members": ["10017310", "21010757", "22011020"], "photos": ["https://i.imgur.com/gHoRKJU.png"], "members_user_ids": ["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ]}
+        assert project_dynamo == {"entity": "project", "code": "PT", "name": "Portfólio", "description": "É um site", "po_user_id": "51ah5jaj-c9jm-1345-666ab-e12341c14a3", "scrum_user_id": "6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "start_date": 1673535600000, "photos": ["https://i.imgur.com/gHoRKJU.png"], "members_user_ids": ["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ]}
 
     def test_project_dynamo_dto_from_dynamo(self):      
-        project_dynamo = {"entity": "project", "code": "PT", "name": "Portfólio", "description": "É um site", "po_user_id": "22011020", "scrum_user_id": "21010757", "start_date": 1673535600000, "members": ["10017310", "21010757", "22011020"], "photos": ["https://i.imgur.com/gHoRKJU.png"], "members_user_ids": ["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ]}
+        project_dynamo = {"entity": "project", "code": "PT", "name": "Portfólio", "description": "É um site", "po_user_id": "51ah5jaj-c9jm-1345-666ab-e12341c14a3", "scrum_user_id": "6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "start_date": 1673535600000, "photos": ["https://i.imgur.com/gHoRKJU.png"], "members_user_ids": ["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ]}
         
         project_dto = ProjectDynamoDTO.from_dynamo(project_dynamo)
         
-        assert project_dto == ProjectDynamoDTO(code="PT", name="Portfólio", description="É um site", po_user_id="22011020", scrum_user_id="21010757", start_date=1673535600000, photos=["https://i.imgur.com/gHoRKJU.png"], members=["10017310", "21010757", "22011020"], members_user_ids=["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ])
+        assert project_dto == ProjectDynamoDTO(code="PT", name="Portfólio", description="É um site", po_user_id="51ah5jaj-c9jm-1345-666ab-e12341c14a3", scrum_user_id="6f5g4h7J-876j-0098-123hb-hgb567fy4hb", start_date=1673535600000, photos=["https://i.imgur.com/gHoRKJU.png"], members_user_ids=["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ])
         
     def test_project_dynamo_dto_to_entity(self):      
-        project_dto = ProjectDynamoDTO(code="PT", name="Portfólio", description="É um site", po_user_id="22011020", scrum_user_id="21010757", start_date=1673535600000, photos=["https://i.imgur.com/gHoRKJU.png"], members=["10017310", "21010757", "22011020"], members_user_ids=["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ])
+        project_dto = ProjectDynamoDTO(code="PT", name="Portfólio", description="É um site", po_user_id="51ah5jaj-c9jm-1345-666ab-e12341c14a3", scrum_user_id="6f5g4h7J-876j-0098-123hb-hgb567fy4hb", start_date=1673535600000, photos=["https://i.imgur.com/gHoRKJU.png"], members_user_ids=["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ])
         
         project = project_dto.to_entity()
         
@@ -44,11 +43,10 @@ class Test_ProjectDynamoDTO:
             code="PT",
             name="Portfólio",
             description="É um site",
-            po_user_id="22011020",
-            scrum_user_id="21010757",
+            po_user_id="51ah5jaj-c9jm-1345-666ab-e12341c14a3",
+            scrum_user_id="6f5g4h7J-876j-0098-123hb-hgb567fy4hb",
             start_date=1673535600000,
             photos=["https://i.imgur.com/gHoRKJU.png"],
-            members=["10017310", "21010757", "22011020"],
             members_user_ids=["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ]
         )
         
@@ -57,21 +55,20 @@ class Test_ProjectDynamoDTO:
             code="PT",
             name="Portfólio",
             description="É um site",
-            po_user_id="22011020",
-            scrum_user_id="21010757",
+            po_user_id="51ah5jaj-c9jm-1345-666ab-e12341c14a3",
+            scrum_user_id="6f5g4h7J-876j-0098-123hb-hgb567fy4hb",
             start_date=1673535600000,
             photos=["https://i.imgur.com/gHoRKJU.png"],
-            members=["10017310", "21010757", "22011020"],
             members_user_ids=["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ]
         )
         
         project_dto = ProjectDynamoDTO.from_entity(project)
         project_dynamo = project_dto.to_dynamo()
         
-        assert project_dynamo == {"entity": "project", "code": "PT", "name": "Portfólio", "description": "É um site", "po_user_id": "22011020", "scrum_user_id": "21010757", "start_date": 1673535600000, "members": ["10017310", "21010757", "22011020"], "photos": ["https://i.imgur.com/gHoRKJU.png"], "members_user_ids": ["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ]}
+        assert project_dynamo == {"entity": "project", "code": "PT", "name": "Portfólio", "description": "É um site", "po_user_id": "51ah5jaj-c9jm-1345-666ab-e12341c14a3", "scrum_user_id": "6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "start_date": 1673535600000, "photos": ["https://i.imgur.com/gHoRKJU.png"], "members_user_ids": ["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ]}
         
     def test_project_dynamo_dto_from_dynamo_to_entity(self):
-        project_dynamo = {"entity": "project", "code": "PT", "name": "Portfólio", "description": "É um site", "po_user_id": "22011020", "scrum_user_id": "21010757", "start_date": 1673535600000, "members": ["10017310", "21010757", "22011020"], "photos": ["https://i.imgur.com/gHoRKJU.png"], "members_user_ids": ["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ]}
+        project_dynamo = {"entity": "project", "code": "PT", "name": "Portfólio", "description": "É um site", "po_user_id": "51ah5jaj-c9jm-1345-666ab-e12341c14a3", "scrum_user_id": "6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "start_date": 1673535600000, "photos": ["https://i.imgur.com/gHoRKJU.png"], "members_user_ids": ["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ]}
         
         project_dto = ProjectDynamoDTO.from_dynamo(project_dynamo)
         project = project_dto.to_entity()
@@ -80,10 +77,9 @@ class Test_ProjectDynamoDTO:
             code="PT",
             name="Portfólio",
             description="É um site",
-            po_user_id="22011020",
-            scrum_user_id="21010757",
+            po_user_id="51ah5jaj-c9jm-1345-666ab-e12341c14a3",
+            scrum_user_id="6f5g4h7J-876j-0098-123hb-hgb567fy4hb",
             start_date=1673535600000,
             photos=["https://i.imgur.com/gHoRKJU.png"],
-            members=["10017310", "21010757", "22011020"],
             members_user_ids=["51ah5jaj-c9jm-1345-666ab-e12341c14a3","6f5g4h7J-876j-0098-123hb-hgb567fy4hb", "93bc6ada-c0d1-7054-66ab-e17414c48ae3" ]
         )
