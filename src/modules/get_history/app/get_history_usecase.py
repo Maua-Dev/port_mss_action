1156ae97abae866bfc57e4c5f9c0176026e8a4da
MateusCMartins724 <mateus.c.martins@outlook.com>
2024-03-19 22:21:33 -0300

    add requester_user to usecase (get_history)

--- src/modules/get_history/app/get_history_usecase.py
+++ src/modules/get_history/app/get_history_usecase.py
@@ -1,13 +1,18 @@
 from typing import Optional, Tuple
 from src.shared.domain.repositories.action_repository_interface import IActionRepository
-from src.shared.helpers.errors.usecase_errors import NoItemsFound
+from src.shared.domain.repositories.member_repository_interface import IMemberRepository
+from src.shared.helpers.errors.usecase_errors import NoItemsFound, UnregisteredUser
 
 
 class GetHistoryUsecase:
-    def __init__(self, repo: IActionRepository):
+    def __init__(self, repo: IActionRepository, repo_member: IMemberRepository):
         self.repo = repo
+        self.repo_member = repo_member
         
     def __call__(self, user_id: str, start: Optional[int] = None, end: Optional[int] = None, exclusive_start_key: Optional[dict] = None, amount: Optional[int] = 20):
+
+        if user_id is None:
+            raise UnregisteredUser()
         
         associated_actions = self.repo.get_associated_actions_by_user_id(user_id=user_id, start=start, end=end, exclusive_start_key=exclusive_start_key, amount=amount)
         
--- tests/modules/get_history/app/test_get_history_usecase.py
+++ tests/modules/get_history/app/test_get_history_usecase.py
@@ -5,12 +5,14 @@ from src.shared.helpers.errors.controller_errors import WrongTypeParameter
 from src.shared.helpers.errors.domain_errors import EntityError
 from src.shared.helpers.errors.usecase_errors import NoItemsFound
 from src.shared.infra.repositories.action_repository_mock import ActionRepositoryMock
+from src.shared.infra.repositories.member_repository_mock import MemberRepositoryMock
 
 
 class Test_GetHistoryUsecase:
     def test_get_history_usecase(self):
         repo = ActionRepositoryMock()
-        usecase = GetHistoryUsecase(repo=repo)
+        repo_member = MemberRepositoryMock()
+        usecase = GetHistoryUsecase(repo=repo, repo_member=repo_member)
         actions, last_evaluated_key = usecase(user_id= '93bc6ada-c0d1-7054-66ab-e17414c48ae3')
         last_action_id = last_evaluated_key[0]
         start_date = last_evaluated_key[1]
@@ -21,7 +23,8 @@ class Test_GetHistoryUsecase:
         
     def test_get_history_usecase_with_start_and_end(self):
         repo = ActionRepositoryMock()
-        usecase = GetHistoryUsecase(repo=repo)
+        repo_member = MemberRepositoryMock()
+        usecase = GetHistoryUsecase(repo=repo, repo_member=repo_member)
         actions, last_evaluated_key = usecase(user_id= '93bc6ada-c0d1-7054-66ab-e17414c48ae3', start=1634526000000, end=1676476000000)
         last_action_id = last_evaluated_key[0]
         start_date = last_evaluated_key[1]
@@ -33,7 +36,8 @@ class Test_GetHistoryUsecase:
         
     def test_get_history_usecase_with_exclusive_start_key(self):
         repo = ActionRepositoryMock()
-        usecase = GetHistoryUsecase(repo=repo)
+        repo_member = MemberRepositoryMock()
+        usecase = GetHistoryUsecase(repo=repo, repo_member=repo_member)
         actions, last_evaluated_key = usecase(user_id= '6f5g4h7J-876j-0098-123hb-hgb567fy4hb', start=1634526000000, end=1676476000000, exclusive_start_key={'action_id' : '87d4a661-0752-4ce2-9440-05e752e636fc', 'start_date' : 1634526000000})
         print(actions, last_evaluated_key)
         last_action_id = last_evaluated_key[0]
@@ -46,7 +50,8 @@ class Test_GetHistoryUsecase:
         
     def test_get_history_usecase_with_amount(self):
         repo = ActionRepositoryMock()
-        usecase = GetHistoryUsecase(repo=repo)
+        repo_member = MemberRepositoryMock()
+        usecase = GetHistoryUsecase(repo=repo, repo_member=repo_member)
         actions, last_evaluated_key = usecase(user_id= '93bc6ada-c0d1-7054-66ab-e17414c48ae3', amount=2)
         last_action_id = last_evaluated_key[0]
         start_date = last_evaluated_key[1]
@@ -58,7 +63,8 @@ class Test_GetHistoryUsecase:
             
     def test_get_history_usecase_no_items_found(self):
         repo = ActionRepositoryMock()
-        usecase = GetHistoryUsecase(repo=repo)
+        repo_member = MemberRepositoryMock()
+        usecase = GetHistoryUsecase(repo=repo, repo_member=repo_member)
         actions, last_evaluated_key = usecase(user_id= 'adbc6ada-c0d1-7054-66ab-e17414c48ae3')
         assert actions == []
         assert last_evaluated_key == None
