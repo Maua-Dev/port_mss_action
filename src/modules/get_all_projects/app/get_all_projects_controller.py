b5a4ebea27e986b262360ac90471f8cb519955c3
MateusCMartins724 <mateus.c.martins@outlook.com>
2024-03-28 18:38:08 -0300

    add requester_user to controller (get_all_projects)

--- src/modules/get_all_projects/app/get_all_projects_controller.py
+++ src/modules/get_all_projects/app/get_all_projects_controller.py
@@ -2,6 +2,8 @@ from src.shared.helpers.external_interfaces.http_codes import OK, InternalServer
 from .get_all_projects_viewmodel import GetAllProjectsViewmodel
 from .get_all_projects_usecase import GetAllProjectsUsecase
 from src.shared.helpers.external_interfaces.external_interface import IRequest, IResponse
+from src.shared.helpers.errors.controller_errors import MissingParameters
+from src.shared.infra.dto.user_api_gateway_dto import UserApiGatewayDTO
 
 
 class GetAllProjectsController:
@@ -10,7 +12,14 @@ class GetAllProjectsController:
     
     def __call__(self, request: IRequest) -> IResponse:
         try:
-            projects = self.usecase()
+            requester_user = request.data.get("requester_user")
+
+            if requester_user is None:
+                MissingParameters("requester_user")
+
+            requester_user = UserApiGatewayDTO.from_api_gateway(requester_user)
+
+            projects = self.usecase(user_id=requester_user.user_id)
             viewmodel = GetAllProjectsViewmodel(projects)
             return OK(viewmodel.to_dict())
         
--- tests/modules/get_all_projects/app/test_get_all_projects_controller.py
+++ tests/modules/get_all_projects/app/test_get_all_projects_controller.py
@@ -2,15 +2,24 @@ from src.modules.get_all_projects.app.get_all_projects_controller import GetAllP
 from src.modules.get_all_projects.app.get_all_projects_usecase import GetAllProjectsUsecase
 from src.shared.helpers.external_interfaces.http_models import HttpRequest
 from src.shared.infra.repositories.action_repository_mock import ActionRepositoryMock
+from src.shared.infra.repositories.member_repository_mock import MemberRepositoryMock
 
 
 class Test_GetAllProjectsController:
     def test_get_all_projects_controller(self):
         
         repo = ActionRepositoryMock()
-        usecase = GetAllProjectsUsecase(repo=repo)
+        repo_member = MemberRepositoryMock()
+        usecase = GetAllProjectsUsecase(repo=repo, repo_member=repo_member)
         controller = GetAllProjectsController(usecase=usecase)
-        request = HttpRequest(body={})
+        request = HttpRequest(body={
+            "requester_user": {
+                "sub": repo_member.members[0].user_id,
+                "name": repo_member.members[0].name,
+                "email": repo_member.members[0].email,
+                "custom:isMaua": True
+            },
+        })
         response = controller(request=request)
         
         assert response.status_code == 200
