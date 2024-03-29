f56b74626da732f9c67dc69beb0f72ca42cf7713
MateusCMartins724 <mateus.c.martins@outlook.com>
2024-03-29 16:07:35 -0300

    add requester_user to create project usecase

--- src/modules/create_project/app/create_project_usecase.py
+++ src/modules/create_project/app/create_project_usecase.py
@@ -1,15 +1,20 @@
 from typing import List
 from src.shared.domain.entities.project import Project
 from src.shared.domain.repositories.action_repository_interface import IActionRepository
-from src.shared.helpers.errors.usecase_errors import DuplicatedItem
+from src.shared.domain.repositories.member_repository_interface import IMemberRepository
+from src.shared.helpers.errors.usecase_errors import DuplicatedItem, UnregisteredUser
 
 
 class CreateProjectUsecase:
-    def __init__(self, repo: IActionRepository):
+    def __init__(self, repo: IActionRepository, repo_member: IMemberRepository):
         self.repo = repo
+        self.repo_member = repo_member
         
-    def __call__(self, code: str, name: str, description: str, po_user_id: str, scrum_user_id: str, start_date: int, members_user_ids: List[str], photos: list = None) -> Project:
+    def __call__(self, user_id: str, code: str, name: str, description: str, po_user_id: str, scrum_user_id: str, start_date: int, members_user_ids: List[str], photos: list = None) -> Project:
         
+        if self.repo_member.get_member(user_id=user_id) is None:
+            raise UnregisteredUser()
+
         project = Project(
             code=code,
             name=name,
