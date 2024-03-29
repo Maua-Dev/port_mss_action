79571fd56fab711e90f81c92b3c36d80f54d1fd6
MateusCMartins724 <mateus.c.martins@outlook.com>
2024-03-29 16:17:42 -0300

    add requester_user to delete_project usecase

--- tests/modules/delete_project/app/test_delete_project_usecase.py
+++ tests/modules/delete_project/app/test_delete_project_usecase.py
@@ -1,28 +1,40 @@
 import pytest
 from src.modules.delete_project.app.delete_project_usecase import DeleteProjectUsecase
 from src.shared.helpers.errors.domain_errors import EntityError
-from src.shared.helpers.errors.usecase_errors import NoItemsFound
+from src.shared.helpers.errors.usecase_errors import NoItemsFound, UnregisteredUser
 from src.shared.infra.repositories.action_repository_mock import ActionRepositoryMock
+from src.shared.infra.repositories.member_repository_mock import MemberRepositoryMock
 
 
 class Test_DeleteProjectUsecase:
     def test_delete_project_usecase(self):
         repo = ActionRepositoryMock()
-        usecase = DeleteProjectUsecase(repo=repo)
+        repo_member = MemberRepositoryMock()
+        usecase = DeleteProjectUsecase(repo=repo, repo_member=repo_member)
         len_before = len(repo.projects)
         
-        project = usecase(code='MF')
+        project = usecase(code='MF', user_id=repo_member.members[0].user_id)
         assert len(repo.projects) == len_before - 1
         assert project.code == 'MF'
         
     def test_delete_project_usecase_no_items_found(self):
         repo = ActionRepositoryMock()
-        usecase = DeleteProjectUsecase(repo=repo)
+        repo_member = MemberRepositoryMock()
+        usecase = DeleteProjectUsecase(repo=repo, repo_member=repo_member)
         with pytest.raises(NoItemsFound):
-            project = usecase(code='VT')
+            project = usecase(code='VT', user_id=repo_member.members[0].user_id)
             
     def test_delete_project_usecase_invalid_code(self):
         repo = ActionRepositoryMock()
-        usecase = DeleteProjectUsecase(repo=repo)
+        repo_member = MemberRepositoryMock()
+        usecase = DeleteProjectUsecase(repo=repo, repo_member=repo_member)
         with pytest.raises(EntityError):
-            project = usecase(code='MauaFood')
+            project = usecase(code='MauaFood', user_id=repo_member.members[0].user_id)
+        
+                
+    def test_delete_project_usecase_invalid_user_id(self):
+        repo = ActionRepositoryMock()
+        repo_member = MemberRepositoryMock()
+        usecase = DeleteProjectUsecase(repo=repo, repo_member=repo_member)
+        with pytest.raises(UnregisteredUser):
+            project = usecase(code='MF', user_id='adsa')
