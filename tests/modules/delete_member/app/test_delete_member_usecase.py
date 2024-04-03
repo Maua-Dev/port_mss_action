729f24f31e6090d28e58dff293c04c3403045566
MateusCMartins724 <mateus.c.martins@outlook.com>
2024-04-02 21:48:15 -0300

    add dual type request delete_member

--- tests/modules/delete_member/app/test_delete_member_usecase.py
+++ tests/modules/delete_member/app/test_delete_member_usecase.py
@@ -2,17 +2,17 @@ import pytest
 from src.modules.delete_member.app.delete_member_usecase import DeleteMemberUseCase
 from src.shared.domain.entities.member import Member
 from src.shared.helpers.errors.domain_errors import EntityError
-from src.shared.helpers.errors.usecase_errors import NoItemsFound
+from src.shared.helpers.errors.usecase_errors import ForbiddenAction, NoItemsFound
 from src.shared.infra.repositories.member_repository_mock import MemberRepositoryMock
 
 
-class Test_DeleteMemberUserCase:
+class Test_DeleteMemberUseCase:
     def test_delete_member_usecase(self):
         repo = MemberRepositoryMock()
         usecase = DeleteMemberUseCase(repo=repo)
         len_before = len(repo.members)
         
-        user_id="93bc6ada-c0d1-7054-66ab-e17414c48ae3"
+        user_id=repo.members[0].user_id
         
         member = usecase(user_id=user_id)
         assert type(member) == Member 
@@ -22,7 +22,7 @@ class Test_DeleteMemberUserCase:
     def test_delete_member_usecase_no_user_id_found(self):
         repo = MemberRepositoryMock()
         usecase = DeleteMemberUseCase(repo=repo)
-        with pytest.raises(NoItemsFound):
+        with pytest.raises(EntityError):
             member = usecase(user_id='93bc6ada-gh46-7054-66ab-e17414c48ae3')
             
     def test_delete_member_usecase_invalid_code(self):
@@ -30,4 +30,41 @@ class Test_DeleteMemberUserCase:
         usecase = DeleteMemberUseCase(repo=repo)
         with pytest.raises(EntityError):
             member = usecase(user_id=123456789)
+    
+    def test_delete_member_usecase_with_member_user_id(self):
+        repo = MemberRepositoryMock()
+        usecase = DeleteMemberUseCase(repo=repo)
+        len_before = len(repo.members)
+        
+        user_id=repo.members[0].user_id
+        member_user_id=repo.members[1].user_id
+        
+        member = usecase(user_id=user_id, member_user_id=member_user_id)
+        assert type(member) == Member 
+        assert len(repo.members) == len_before - 1
+        assert member.user_id == member_user_id
+    
+    def test_delete_member_usecase_forbidden_user_with_member_user_id(self):
+        repo = MemberRepositoryMock()
+        usecase = DeleteMemberUseCase(repo=repo)
+        len_before = len(repo.members)
+        
+        user_id=repo.members[2].user_id
+        member_user_id=repo.members[1].user_id
+
+
+        with pytest.raises(ForbiddenAction):
+            member = usecase(user_id=user_id, member_user_id=member_user_id)
+    
+    def test_delete_member_usecase_forbidden_user_with_member_user_id(self):
+        repo = MemberRepositoryMock()
+        usecase = DeleteMemberUseCase(repo=repo)
+        len_before = len(repo.members)
+        
+        user_id=repo.members[2].user_id
+        member_user_id=repo.members[1].user_id
+
+
+        with pytest.raises(ForbiddenAction):
+            member = usecase(user_id=user_id, member_user_id=member_user_id)
           
