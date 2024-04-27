46e08a8b3f8a8412a3ddc2182d19cff5f0956182
Rubiozito <rafaelrubiocarnes@gmail.com>
2024-04-27 01:12:51 -0300

    delete action presenter

---
+++ tests/modules/delete_action/app/test_delete_action_presenter.py
@@ -0,0 +1,251 @@
+import json
+from src.modules.delete_action.app.delete_action_presenter import lambda_handler
+from src.shared.infra.repositories.member_repository_mock import MemberRepositoryMock
+
+member = MemberRepositoryMock().members[0]
+
+class Test_DeleteActionPresenter:
+    def test_delete_action_presenter(self):
+        event = {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/my/path",
+            "rawQueryString": "parameter1=value1&parameter1=value2&parameter2=value",
+            "cookies": [
+                "cookie1",
+                "cookie2"
+            ],
+            "headers": {
+                "header1": "value1",
+                "header2": "value1,value2"
+            },
+            "queryStringParameters": {
+                "parameter1": "1"
+            },
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": {
+                    "claims": 
+                    {
+                            "sub": member.user_id, 
+                            "name": member.name,
+                            "email": member.email,
+                            "custom:isMaua": True
+                    }
+                },
+                "domainName": "<url-id>.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "<url-id>",
+                "external_interfaces": {
+                    "method": "POST",
+                    "path": "/my/path",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent"
+                },
+                "requestId": "id",
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390
+            },
+            "body": '{ "action_id": "5f4f13df-e7d3-4a10-9219-197ceae9e3f0"}',
+            "pathParameters": None,
+            "isBase64Encoded": None,
+            "stageVariables": None
+        }
+        
+        response = lambda_handler(event, None)
+
+        expected = {
+            'action':{
+                'user_id':'6f5g4h7J-876j-0098-123hb-hgb567fy4hb',
+                'start_date':1644256000000,
+                'end_date':1653756000000,
+                'duration':9500000000,
+                'action_id':'5f4f13df-e7d3-4a10-9219-197ceae9e3f0',
+                'is_valid':True,
+                'story_id':94,
+                'title':'Retrospectiva',
+                'description':'Revisão de sprint',
+                'project_code':'PT',
+                'associated_members_user_ids':["75648hbr-184n-1985-91han-7ghn4HgF182", "51ah5jaj-c9jm-1345-666ab-e12341c14a3", "76h35dg4-h76v-1875-987hn-h67gfv45Gt4", "93bc6ada-c0d1-7054-66ab-e17414c48ae3", "6574hgyt-785n-9134-18gn4-7gh5uvn36cG", "7gh5yf5H-857H-1234-75hng-94832hvng1s"],
+                'stack_tags':['INFRA'],
+                'action_type_tag':'CODEREVIEW'
+            },
+            'message':'the action was deleted for all the members in this action'
+            }
+        
+        assert response["statusCode"] == 200
+        assert json.loads(response["body"]) == expected
+
+
+    def test_delete_action_presenter_missing_parameters(self):
+        event = {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/my/path",
+            "rawQueryString": "parameter1=value1&parameter1=value2&parameter2=value",
+            "cookies": [
+                "cookie1",
+                "cookie2"
+            ],
+            "headers": {
+                "header1": "value1",
+                "header2": "value1,value2"
+            },
+            "queryStringParameters": {
+                "parameter1": "1"
+            },
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": {
+                    "claims":
+                        {
+                            "sub": member.user_id,
+                            "name": member.name,
+                            "email": member.email,
+                            "custom:isMaua": True
+                        }
+                },
+                "domainName": "<url-id>.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "<url-id>",
+                "external_interfaces": {
+                    "method": "POST",
+                    "path": "/my/path",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent"
+                },
+                "requestId": "id",
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390
+            },
+            "body": '{}',
+            "pathParameters": None,
+            "isBase64Encoded": None,
+            "stageVariables": None
+        }
+        
+        response = lambda_handler(event, None)
+        
+        assert response["statusCode"] == 400
+        assert json.loads(response["body"]) == 'Field action_id is missing'
+
+    def test_delete_action_presenter_entity_error(self):
+        event = {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/my/path",
+            "rawQueryString": "parameter1=value1&parameter1=value2&parameter2=value",
+            "cookies": [
+                "cookie1",
+                "cookie2"
+            ],
+            "headers": {
+                "header1": "value1",
+                "header2": "value1,value2"
+            },
+            "queryStringParameters": {
+                "parameter1": "1"
+            },
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": {
+                    "claims":
+                        {
+                            "sub": member.user_id,
+                            "name": member.name,
+                            "email": member.email,
+                            "custom:isMaua": True
+                        }
+                },
+                "domainName": "<url-id>.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "<url-id>",
+                "external_interfaces": {
+                    "method": "POST",
+                    "path": "/my/path",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent"
+                },
+                "requestId": "id",
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390
+            },
+            "body": '{"action_id": "PORTAL-INFERNO"}',
+            "pathParameters": None,
+            "isBase64Encoded": None,
+            "stageVariables": None
+        }
+        
+        response = lambda_handler(event, None)
+
+        assert response["statusCode"] == 400
+        assert json.loads(response["body"]) == 'Field action_id is not valid'
+
+
+    def test_delete_action_presenter_not_found(self):
+        event = {
+            "version": "2.0",
+            "routeKey": "$default",
+            "rawPath": "/my/path",
+            "rawQueryString": "parameter1=value1&parameter1=value2&parameter2=value",
+            "cookies": [
+                "cookie1",
+                "cookie2"
+            ],
+            "headers": {
+                "header1": "value1",
+                "header2": "value1,value2"
+            },
+            "queryStringParameters": {
+                "parameter1": "1"
+            },
+            "requestContext": {
+                "accountId": "123456789012",
+                "apiId": "<urlid>",
+                "authentication": None,
+                "authorizer": {
+                    "claims":
+                        {
+                            "sub": member.user_id,
+                            "name": member.name,
+                            "email": member.email,
+                            "custom:isMaua": True
+                        }
+                },
+                "domainName": "<url-id>.lambda-url.us-west-2.on.aws",
+                "domainPrefix": "<url-id>",
+                "external_interfaces": {
+                    "method": "POST",
+                    "path": "/my/path",
+                    "protocol": "HTTP/1.1",
+                    "sourceIp": "123.123.123.123",
+                    "userAgent": "agent"
+                },
+                "requestId": "id",
+                "routeKey": "$default",
+                "stage": "$default",
+                "time": "12/Mar/2020:19:03:58 +0000",
+                "timeEpoch": 1583348638390
+            },
+            "body": '{"action_id": "5f4f13df-e7d3-4a10-9219-197ceae9e3f1"}',
+            "pathParameters": None,
+            "isBase64Encoded": None,
+            "stageVariables": None
+        }
+        
+        response = lambda_handler(event, None)
+        
+        assert response["statusCode"] == 404
+        assert json.loads(response["body"]) == 'No items found for action_id'
