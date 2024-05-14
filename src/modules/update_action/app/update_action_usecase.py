a9bd65dd366641464d687cb7e6e220914694a51f
MateusCMartins724 <mateus.c.martins@outlook.com>
2024-05-13 22:24:12 -0300

    update_action fix

--- src/modules/update_action/app/update_action_usecase.py
+++ src/modules/update_action/app/update_action_usecase.py
@@ -14,7 +14,7 @@ class UpdateActionUsecase:
         self.repo_member = repo_member
         
     def __call__(self, action_id: str,
-                new_user_id: str, 
+                user_id: str, 
                 new_start_date: Optional[int] = None, 
                 new_end_date: Optional[int] = None, 
                 new_duration: Optional[int] = None, 
@@ -24,35 +24,27 @@ class UpdateActionUsecase:
                 new_project_code: Optional[str] = None, 
                 new_associated_members_user_ids: Optional[List[str]] = None, 
                 new_stack_tags: Optional[List[STACK]] = None, 
-                new_action_type_tag: Optional[ACTION_TYPE] = None,
-                new_member_user_id: Optional[str] = None) -> Action:
+                new_action_type_tag: Optional[ACTION_TYPE] = None) -> Action:
         
-        user = self.repo_member.get_member(new_user_id)
+        user = self.repo_member.get_member(user_id)
         if user is None:
             raise UnregisteredUser()  
-
-        if new_member_user_id is not None:
-            if self.repo_member.get_member(new_member_user_id) is None:
-                raise UnregisteredUser()  
         
         action = self.repo.get_action(action_id)
         if not action:
             raise NoItemsFound('action')
         
         is_admin = Member.validate_role_admin(user.role)
-        if is_admin == False and new_user_id != action.user_id:
+        if is_admin == False and user_id != action.user_id:
             raise ForbiddenAction('This user can´t update this action. He is not the owner of the action or an admin.')
         
         members = None
+        if new_associated_members_user_ids is not None:
+            members = new_associated_members_user_ids + action.associated_members_user_ids
+        else:
+            members = action.associated_members_user_ids
+
         start_date = new_start_date if new_start_date is not None else action.start_date
-        if new_associated_members_user_ids and new_user_id and new_member_user_id:
-            members = [new_member_user_id] + new_associated_members_user_ids
-        elif new_associated_members_user_ids and new_user_id:
-            members = [new_user_id] + new_associated_members_user_ids
-        elif new_member_user_id and new_user_id:
-            members = [new_member_user_id] + action.associated_members_user_ids
-        elif new_user_id:
-            members = [new_user_id] + action.associated_members_user_ids
         if members != None and set(members) != set([action.user_id] + action.associated_members_user_ids):
             self.repo.batch_update_associated_action_members(action_id, members, start_date=start_date)
         elif start_date != action.start_date:
@@ -61,12 +53,5 @@ class UpdateActionUsecase:
         description = new_description if new_description != '' else action.description
         story_id = new_story_id if new_story_id != -1 else action.story_id
 
-        if is_admin and new_member_user_id is None:
-            return self.repo.update_action(action_id=action_id, new_user_id=new_user_id, new_start_date=new_start_date, new_end_date=new_end_date, new_duration=new_duration, new_story_id=story_id, new_title=new_title, new_description=description, new_project_code=new_project_code, new_associated_members_user_ids=new_associated_members_user_ids, new_stack_tags=new_stack_tags, new_action_type_tag=new_action_type_tag)
-        elif is_admin and new_member_user_id is not None:
-            return self.repo.update_action(action_id=action_id, new_user_id=new_member_user_id, new_start_date=new_start_date, new_end_date=new_end_date, new_duration=new_duration, new_story_id=story_id, new_title=new_title, new_description=description, new_project_code=new_project_code, new_associated_members_user_ids=new_associated_members_user_ids, new_stack_tags=new_stack_tags, new_action_type_tag=new_action_type_tag)
-        elif not is_admin and new_member_user_id is None:
-            return self.repo.update_action(action_id=action_id, new_user_id=new_user_id, new_start_date=new_start_date, new_end_date=new_end_date, new_duration=new_duration, new_story_id=story_id, new_title=new_title, new_description=description, new_project_code=new_project_code, new_associated_members_user_ids=new_associated_members_user_ids, new_stack_tags=new_stack_tags, new_action_type_tag=new_action_type_tag)
-        else:
-            raise ForbiddenAction('user. Not allowed to update action of another user.')
+        return self.repo.update_action(action_id=action_id, new_user_id=action.user_id, new_start_date=new_start_date, new_end_date=new_end_date, new_duration=new_duration, new_story_id=story_id, new_title=new_title, new_description=description, new_project_code=new_project_code, new_associated_members_user_ids=members, new_stack_tags=new_stack_tags, new_action_type_tag=new_action_type_tag)
         
--- tests/modules/update_action/app/test_update_action_usecase.py
+++ tests/modules/update_action/app/test_update_action_usecase.py
@@ -13,7 +13,7 @@ class Test_UpdateActionUsecase:
         repo_member = MemberRepositoryMock()
         usecase = UpdateActionUsecase(repo=repo, repo_member=repo_member)
 
-        action = usecase(action_id=repo.actions[0].action_id, new_user_id='51ah5jaj-c9jm-1345-666ab-e12341c14a3', new_start_date=1634526000000, new_story_id=100, new_associated_members_user_ids=['51ah5jaj-c9jm-1345-666ab-e12341c14a3'], new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE)
+        action = usecase(action_id=repo.actions[0].action_id, user_id='51ah5jaj-c9jm-1345-666ab-e12341c14a3', new_start_date=1634526000000, new_story_id=100, new_associated_members_user_ids=['51ah5jaj-c9jm-1345-666ab-e12341c14a3'], new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE)
         
         assert repo.actions[0] == action
         assert all(action.user_id in ['51ah5jaj-c9jm-1345-666ab-e12341c14a3'] for action in repo.associated_actions if action.action_id == repo.actions[0].action_id)
@@ -25,17 +25,17 @@ class Test_UpdateActionUsecase:
         usecase = UpdateActionUsecase(repo=repo, repo_member=repo_member)
 
         with pytest.raises(NoItemsFound):
-            action = usecase(action_id='01538473-35ea-43c5-a823-f278c3fb42f3', new_user_id='51ah5jaj-c9jm-1345-666ab-e12341c14a3', new_start_date=1634526000000, new_story_id=100, new_associated_members_user_ids=['19017311'], new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE)
+            action = usecase(action_id='01538473-35ea-43c5-a823-f278c3fb42f3', user_id='51ah5jaj-c9jm-1345-666ab-e12341c14a3', new_start_date=1634526000000, new_story_id=100, new_associated_members_user_ids=['19017311'], new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE)
 
     def test_update_action_usecase_another_member(self):
         repo = ActionRepositoryMock()
         repo_member = MemberRepositoryMock()
         usecase = UpdateActionUsecase(repo=repo, repo_member=repo_member)
 
-        action = usecase(action_id=repo.actions[0].action_id, new_user_id=repo_member.members[0].user_id, new_start_date=1634526000000, new_story_id=100, new_associated_members_user_ids=['51ah5jaj-c9jm-1345-666ab-e12341c14a3'], new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE, new_member_user_id=repo_member.members[2].user_id)
+        action = usecase(action_id=repo.actions[0].action_id, user_id=repo_member.members[0].user_id, new_start_date=1634526000000, new_story_id=100, new_associated_members_user_ids=['51ah5jaj-c9jm-1345-666ab-e12341c14a3'], new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE)
         
         assert repo.actions[0] == action
-        assert all(action.user_id in [repo_member.members[2].user_id] for action in repo.associated_actions if action.action_id == repo.actions[0].action_id)
+        assert all(action.user_id in [action.user_id] for action in repo.associated_actions if action.action_id == repo.actions[0].action_id)
         assert all(action.start_date == 1634526000000 for action in repo.associated_actions if action.action_id == repo.actions[0].action_id)
 
     def test_update_action_usecase_another_member_no_new_associated_members(self):
@@ -43,10 +43,10 @@ class Test_UpdateActionUsecase:
         repo_member = MemberRepositoryMock()
         usecase = UpdateActionUsecase(repo=repo, repo_member=repo_member)
 
-        action = usecase(action_id=repo.actions[0].action_id, new_user_id=repo_member.members[0].user_id, new_start_date=1634526000000, new_story_id=100, new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE, new_member_user_id=repo_member.members[2].user_id)
+        action = usecase(action_id=repo.actions[0].action_id, user_id=repo_member.members[0].user_id, new_start_date=1634526000000, new_story_id=100, new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE)
         
         assert repo.actions[0] == action
-        assert all(action.user_id in [repo_member.members[2].user_id] for action in repo.associated_actions if action.action_id == repo.actions[0].action_id)
+        assert all(action.user_id in [action.user_id] for action in repo.associated_actions if action.action_id == repo.actions[0].action_id)
         assert all(action.start_date == 1634526000000 for action in repo.associated_actions if action.action_id == repo.actions[0].action_id)
 
     def test_update_action_usecase_only_user(self):
@@ -54,10 +54,10 @@ class Test_UpdateActionUsecase:
         repo_member = MemberRepositoryMock()
         usecase = UpdateActionUsecase(repo=repo, repo_member=repo_member)
 
-        action = usecase(action_id=repo.actions[0].action_id, new_user_id=repo_member.members[0].user_id, new_start_date=1634526000000, new_story_id=100, new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE)
+        action = usecase(action_id=repo.actions[0].action_id, user_id=repo_member.members[0].user_id, new_start_date=1634526000000, new_story_id=100, new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE)
         
         assert repo.actions[0] == action
-        assert all(action.user_id in [repo_member.members[0].user_id] for action in repo.associated_actions if action.action_id == repo.actions[0].action_id)
+        assert all(action.user_id in [action.user_id] for action in repo.associated_actions if action.action_id == repo.actions[0].action_id)
         assert all(action.start_date == 1634526000000 for action in repo.associated_actions if action.action_id == repo.actions[0].action_id)
 
     def test_update_action_usecase_another_member_forbidden_action(self):
@@ -65,5 +65,5 @@ class Test_UpdateActionUsecase:
         repo_member = MemberRepositoryMock()
         usecase = UpdateActionUsecase(repo=repo, repo_member=repo_member)
         with pytest.raises(ForbiddenAction):
-            action = usecase(action_id=repo.actions[0].action_id, new_user_id=repo_member.members[2].user_id, new_start_date=1634526000000, new_story_id=100, new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE, new_member_user_id=repo_member.members[0].user_id)
+            action = usecase(action_id=repo.actions[0].action_id, user_id=repo_member.members[2].user_id, new_start_date=1634526000000, new_story_id=100, new_title='Teste', new_end_date=1634536800000, new_project_code='MF', new_stack_tags=[STACK.BACKEND], new_action_type_tag=ACTION_TYPE.CODE)
     
