7f1a933a2141b8c38098c3fa96fc0a5c3c2b1cda
mateus <22.01082-3@maua.br>
2024-09-12 21:49:22 -0300

    print test

--- src/shared/domain/entities/action.py
+++ src/shared/domain/entities/action.py
@@ -57,7 +57,9 @@ class Action(abc.ABC):
         if type(associated_members_user_ids) == list:
             if not all([self.validate_user_id(user_id) for user_id in associated_members_user_ids]):
                 raise EntityError('associated_members_user_ids')
-            if str(user_id) in associated_members_user_ids:
+            if user_id in associated_members_user_ids:
+                print(user_id, associated_members_user_ids)
+                print(user_id in associated_members_user_ids)
                 raise EntityError('associated_members_user_ids')
             if len(associated_members_user_ids) != len(set(associated_members_user_ids)):
                 raise EntityError('associated_members_user_ids')
