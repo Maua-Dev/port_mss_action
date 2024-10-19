72fdd65fe3018f0428f2c9c42fbad290fbed7724
mateus <22.01082-3@maua.br>
2024-10-19 14:04:25 -0300

    feat: add key to put_object

--- src/shared/infra/repositories/member_repository_dynamo.py
+++ src/shared/infra/repositories/member_repository_dynamo.py
@@ -46,7 +46,6 @@ class MemberRepositoryDynamo(IMemberRepository):
         self.S3_BUCKET_NAME = Environments.get_envs().s3_bucket_name
         
     def create_member(self, member: Member) -> Member:
-        print(member.photo)
         url = self.upload_member_photo(member.user_id, member.photo)
         member.photo = url
         item = MemberDynamoDTO.from_entity(member).to_dynamo()
@@ -239,9 +238,9 @@ class MemberRepositoryDynamo(IMemberRepository):
 
             self.s3_client.put_object(
                 Bucket=self.S3_BUCKET_NAME,
-                Key=f"{user}",
+                Key=f"{user_id}",
                 Body=photo_bytes,
-                ContentType="image/png"
+                ContentType="image/jpeg"
             )
 
             return s3_key
