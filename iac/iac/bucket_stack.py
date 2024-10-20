3a40ca13aa59250bd284dfbe74e8525a4e369764
mateus <22.01082-3@maua.br>
2024-10-20 18:08:14 -0300

    feat: add project bucket

--- iac/iac/bucket_stack.py
+++ iac/iac/bucket_stack.py
@@ -23,7 +23,7 @@ class BucketStack(Construct):
 
         REMOVAL_POLICY = RemovalPolicy.RETAIN if 'prod' in self.github_ref_name else RemovalPolicy.DESTROY
 
-        self.s3_bucket = aws_s3.Bucket(self, "PortalInterno_Member_Photo_S3_Bucket",
+        self.s3_bucket_member = aws_s3.Bucket(self, "PortalInterno_Member_Photo_S3_Bucket",
                                        versioned=True,
                                        block_public_access=aws_s3.BlockPublicAccess.BLOCK_ALL,
                                        event_bridge_enabled=False,
@@ -42,7 +42,7 @@ class BucketStack(Construct):
         
         self.s3_bucket.grant_read_write(oai)
 
-        self.cloudfront_distribution = aws_cloudfront.Distribution(self, "PortalInterno_Member_Photo_CloudFront_Distribution",
+        self.cloudfront_distribution_project = aws_cloudfront.Distribution(self, "PortalInterno_Member_Photo_CloudFront_Distribution",
                                                                      default_behavior=aws_cloudfront.BehaviorOptions(
                                                                           origin=aws_cloudfront_origins.S3Origin(
                                                                             self.s3_bucket,
@@ -54,4 +54,36 @@ class BucketStack(Construct):
                                                                           allowed_methods=aws_cloudfront.AllowedMethods.ALLOW_ALL
                                                                      )
                                                                      )
+        
+        self.s3_bucket_project = aws_s3.Bucket(self, "PortalInterno_Project_Photo_S3_Bucket",
+                                versioned=True,
+                                block_public_access=aws_s3.BlockPublicAccess.BLOCK_ALL,
+                                event_bridge_enabled=False,
+                                cors=[aws_s3.CorsRule(
+                                        allowed_methods=[
+                                            aws_s3.HttpMethods.GET, aws_s3.HttpMethods.PUT, aws_s3.HttpMethods.POST],
+                                        allowed_origins=["*"],
+                                        allowed_headers=["*"],
+                                        max_age=3000
+                                        )],
+                                removal_policy=REMOVAL_POLICY
+                                )
+
+        oai = aws_cloudfront.OriginAccessIdentity(self, "PortalInterno_Project_Photo_OAI",
+                                                  comment="This is PortalInterno project photo OAI")
+        
+        self.s3_bucket.grant_read_write(oai)
+
+        self.cloudfront_distribution_project = aws_cloudfront.Distribution(self, "PortalInterno_Project_Photo_CloudFront_Distribution",
+                                                                     default_behavior=aws_cloudfront.BehaviorOptions(
+                                                                          origin=aws_cloudfront_origins.S3Origin(
+                                                                            self.s3_bucket,
+                                                                            origin_access_identity=oai),
+                                                                          origin_request_policy=aws_cloudfront.OriginRequestPolicy.CORS_S3_ORIGIN,
+                                                                          viewer_protocol_policy=aws_cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+                                                                          response_headers_policy=aws_cloudfront.ResponseHeadersPolicy.CORS_ALLOW_ALL_ORIGINS,
+                                                                          cache_policy=aws_cloudfront.CachePolicy.CACHING_OPTIMIZED,
+                                                                          allowed_methods=aws_cloudfront.AllowedMethods.ALLOW_ALL
+                                                                     )
+                                            )
 
