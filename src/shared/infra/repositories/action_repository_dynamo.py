52126567ec231a2bf33a72508092a777f38ecd7a
LucasCrapino <163664488+LucasCrapino@users.noreply.github.com>
2024-10-30 21:47:00 -0300

    fix: add project_code

--- src/shared/infra/repositories/action_repository_dynamo.py
+++ src/shared/infra/repositories/action_repository_dynamo.py
@@ -506,9 +506,9 @@ class ActionRepositoryDynamo(IActionRepository):
         for item in resp['Items']:
             action = ActionDynamoDTO.from_dynamo(item).to_entity()
 
-        if action.user_id in durations_by_project:
-            durations_by_project[action.project] += action.duration
+        if action.project_code in durations_by_project:
+            durations_by_project[action.project_code] += action.duration
         else:
-            durations_by_project[action.project] = action.duration
+            durations_by_project[action.project_code] = action.duration
         
         return durations_by_project
