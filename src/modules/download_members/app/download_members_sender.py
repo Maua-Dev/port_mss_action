c826b8f385c42d29ba73f60d5d86babfb422baa6
Lucas Crapino <lucascrapino@gmail.com>
2025-04-24 15:55:25 -0300

    fix: change download_members_sender

--- src/modules/download_members/app/download_members_sender.py
+++ src/modules/download_members/app/download_members_sender.py
@@ -1,21 +1,44 @@
-import boto3
-from .download_members_extractor import DownloadMembersExtractor
-from .download_members_transformer import DownloadMembersTransformer
-from src.shared.infra.repositories.member_repository_mock import MemberRepositoryMock
+import datetime
+from download_members_extractor import DownloadMembersExtractor
+from download_members_transformer import DownloadMembersTransformer
+from src.shared.environments import Environments
+from src.shared.infra.repositories import S3Manager
 
 
-repo_member = MemberRepositoryMock()
-extractor = DownloadMembersExtractor(repo=repo_member)
-transformer = DownloadMembersTransformer(extractor=extractor)
+repo = Environments.get_envs().get_member_repo()()
 
 def lambda_handler(event,context):
-    file_content = transformer()
-    
-    return {
-        'statusCode': 200,
-        'body': file_content.getvalue(),
-        'headers': {
-            'Content-Type': 'text/csv',
-            'Content-Disposition': 'attachment; filename=membros.csv'
-        },
-    }
+    extractor = DownloadMembersExtractor(repo)
+
+    current_date = datetime.datetime.now()
+
+    year = current_date.year
+    month = current_date.month
+    day = current_date.day
+
+    file_name = f"relatorio_gerado_em_{day}_{month}_{year}.xlsx"
+    file_path = f"relatorios/"
+
+    extractor = DownloadMembersExtractor(member_repository=repo)
+    transformer = DownloadMembersTransformer(extractor)
+    download = transformer()
+
+    if download:
+
+        bucket_manager = S3Manager()
+
+        try:
+
+            response = bucket_manager.upload_file(key=file_path + file_name,
+                                                   file_type=".xlsx",
+                                                   decode_string=download,
+                                                   )
+            print("Download generated and uploaded successfully")
+            return 1
+        except:
+
+            raise Exception("Error uploading file to S3")
+        
+    else:
+
+        raise Exception("Error generating download")
