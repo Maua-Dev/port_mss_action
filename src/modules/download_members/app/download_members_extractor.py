2ee8546f7b41c8427ea450b5ffbc5a0ceae74d79
LucasCrapino <163664488+LucasCrapino@users.noreply.github.com>
2025-04-24 13:58:46 -0300

    feat: add members_dict

--- src/modules/download_members/app/download_members_extractor.py
+++ src/modules/download_members/app/download_members_extractor.py
@@ -13,4 +13,22 @@ class DownloadMembersExtractor:
         except:
             raise NoItemsFound('members')
         
-        return members
+        members_dict = [{
+                "name": m.name,
+                "email_dev": m.email_dev,
+                "email": m.email,
+                "ra": m.ra,
+                "role": m.role.name if hasattr(m.role, 'name') else str(m.role),
+                "stack": m.stack.name if hasattr(m.stack, 'name') else str(m.stack),
+                "year": m.year,
+                "cellphone": m.cellphone,
+                "course": m.course.name if hasattr(m.course, 'name') else str(m.course),
+                "hired_date": m.hired_date,
+                "active": m.active.name if hasattr(m.active, 'name') else str(m.active),
+                "deactivated_date": m.deactivated_date,
+                "user_id": m.user_id,
+                "photo": m.photo
+            }
+            for m in members
+        ]
+        return members_dict
