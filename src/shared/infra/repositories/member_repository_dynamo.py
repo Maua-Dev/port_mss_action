50d2ed4e3995c6f8330721a4e2c005d51e71f11f
Rodrigo Morales <109971335+RodrigoM2004@users.noreply.github.com>
2025-04-29 11:34:25 -0300

    Revert "Merge branch homolog -> prod - Criado Automatico"

--- src/shared/infra/repositories/member_repository_dynamo.py
+++ src/shared/infra/repositories/member_repository_dynamo.py
@@ -1,11 +1,7 @@
-import base64
-import datetime
-import imghdr
-from src.shared.helpers.errors.controller_errors import WrongTypeFile
+import os
 from typing import List, Optional
 from src.shared.domain.repositories.member_repository_interface import IMemberRepository
 from src.shared.domain.entities.member import Member
-from botocore.config import Config
 from src.shared.infra.dto.member_dynamo_dto import MemberDynamoDTO
 from src.shared.environments import Environments
 from src.shared.infra.external.dynamo.datasources.dynamo_datasource import DynamoDatasource
@@ -33,23 +29,10 @@ class MemberRepositoryDynamo(IMemberRepository):
             dynamo_table_name=Environments.get_envs().dynamo_table_name_member,
             region=Environments.get_envs().region,
             partition_key=Environments.get_envs().dynamo_partition_key,
-            sort_key=Environments.get_envs().dynamo_sort_key)
-        
-        my_config = Config(
-            region_name=Environments.get_envs().region,
-            signature_version='s3v4',
+            sort_key=Environments.get_envs().dynamo_sort_key,
         )
-        self.s3_client = boto3.client(
-            's3', config=my_config, region_name=Environments.get_envs().region)
-        
-        self.cloud_front_distribution_domain_assets_member = Environments.get_envs().cloud_front_distribution_domain_assets_member
-
-        self.S3_BUCKET_NAME = Environments.get_envs().s3_bucket_name_member
         
     def create_member(self, member: Member) -> Member:
-        if member.photo is not None:
-            url = self.upload_member_photo(member.user_id, member.photo)
-            member.photo = url
         item = MemberDynamoDTO.from_entity(member).to_dynamo()
         resp = self.dynamo.put_item(item=item, partition_key=self.member_partition_key_format(member), sort_key=self.member_sort_key_format(member.user_id), is_decimal=True)
         
@@ -95,7 +78,7 @@ class MemberRepositoryDynamo(IMemberRepository):
         
         return MemberDynamoDTO.from_dynamo(delete_member["Attributes"]).to_entity()
     
-    def update_member(self, user_id: str, new_name: Optional[str] = None, new_email_dev: Optional[str] = None, new_role: Optional[str] = None, new_stack: Optional[str] = None, new_year: Optional[int] = None, new_cellphone: Optional[str] = None, new_course: Optional[str] = None, new_active: Optional[str] = None, new_deactivated_date: Optional[int] = None,  new_photo: Optional[str] = None) -> Member:
+    def update_member(self, user_id: str, new_name: Optional[str] = None, new_email_dev: Optional[str] = None, new_role: Optional[str] = None, new_stack: Optional[str] = None, new_year: Optional[int] = None, new_cellphone: Optional[str] = None, new_course: Optional[str] = None, new_active: Optional[str] = None, new_deactivated_date: Optional[int] = None, new_photo: Optional[str] = None) -> Member:
         member_to_update = self.get_member(user_id=user_id)
         
         if member_to_update is None:
@@ -120,14 +103,7 @@ class MemberRepositoryDynamo(IMemberRepository):
         if new_deactivated_date is not None:
             member_to_update.deactivated_date = new_deactivated_date
         if new_photo is not None:
-            if new_photo[:5] == "https":
-                url = new_photo
-            else:
-                if member_to_update.photo is not None:
-                    self.s3_client.delete_object(Bucket=self.S3_BUCKET_NAME, Key=self.generate_key(user_id, file_type=member_to_update.photo[74:77]))
-                url = self.upload_member_photo(user_id, new_photo)
-            member_to_update.photo = url
-            
+            member_to_update.photo = new_photo
         update_dict ={
             "name": member_to_update.name,
             "email_dev": member_to_update.email_dev,
@@ -138,7 +114,7 @@ class MemberRepositoryDynamo(IMemberRepository):
             "course": member_to_update.course.value,
             "active": member_to_update.active.value,
             "deactivated_date": member_to_update.deactivated_date if new_deactivated_date is not None else None,
-            "photo": url if new_photo is not None else None
+            "photo": member_to_update.photo
         }
         
         resp = self.dynamo.update_item(partition_key=self.member_partition_key_format(member_to_update), sort_key=self.member_sort_key_format(user_id), update_dict=update_dict)
@@ -187,52 +163,3 @@ class MemberRepositoryDynamo(IMemberRepository):
         except Exception as err:
             print(err)
             return False
-    
-    def generate_key(self, user_id: str, file_type: str) -> str:
-
-        key = f"{user_id}.{file_type}"
-        return key
-    
-    def upload_member_photo(self, user_id: str, photo: str) -> str:
-        try:
-            photo_bytes = base64.b64decode(photo)
-            
-
-            file_type = imghdr.what(None, photo_bytes)
-            if file_type is None:
-                raise WrongTypeFile()
-            
-            content_type = f"'image/{file_type}"
-
-            s3_key = self.generate_key(user_id, file_type)
-
-            self.s3_client.put_object(
-                Bucket=self.S3_BUCKET_NAME,
-                Key=f"{s3_key}",
-                Body=photo_bytes,
-                ContentType= content_type
-            )
-
-            meta = {
-                "user_id": user_id,
-                "time_created": str(datetime.datetime.now().timestamp() * 1000)
-            }
-
-            presigned_url = self.s3_client.generate_presigned_url(
-                ClientMethod='put_object',
-                Params={
-                    'Bucket': self.S3_BUCKET_NAME,
-                    'Key': s3_key,
-                    'Metadata': meta,
-                    'ContentDisposition': 'inline',
-                },
-                ExpiresIn=600,
-            )
-
-            presigned_url = presigned_url.replace(
-                f"{self.S3_BUCKET_NAME}.s3.amazonaws.com", self.cloud_front_distribution_domain_assets_member)
-
-            return presigned_url
-        except Exception as e:
-            print(e)
-            raise e
