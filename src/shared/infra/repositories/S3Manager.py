90e7229518ba2430f4083dfcc120d377d3c45877
Lucas Crapino <lucascrapino@gmail.com>
2025-05-08 16:20:52 -0300

    feat: creat bucket for members report

--- src/shared/infra/repositories/S3Manager.py
+++ src/shared/infra/repositories/S3Manager.py
@@ -6,22 +6,12 @@ class S3Manager:
 
     def __init__(self):
         self.__envs = Environments.get_envs()
-        stage = self.__envs.stage.value
-        if stage == "TEST":
-            self.s3 = boto3.client(
-                "s3",
-                aws_access_key_id=self.__envs.client_id,
-                endpoint_url=self.__envs.bucket_endpoint_url,
-                region_name=self.__envs.region,
-                config=boto3.session.Config(signature_version="s3v4")
-            )
-        else:
-            self.s3 = boto3.client("s3")
+        self.s3 = boto3.client("s3")
 
     def upload_file(self, key, file_type, decode_string):
 
         response = self.s3.put_object(
-            Bucket = self.__envs.s3_bucket_name,
+            Bucket = self.__envs.s3_bucket_name_member_report,
             Key = key,
             Body = decode_string,
             ContentType = file_type.replace(".",""),
