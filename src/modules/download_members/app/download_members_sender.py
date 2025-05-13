87da15719456a606c6dc576539c46ad608b6c2f9
LucasCrapino <163664488+LucasCrapino@users.noreply.github.com>
2025-05-13 12:45:57 -0300

    fix: fix imports

--- src/modules/download_members/app/download_members_sender.py
+++ src/modules/download_members/app/download_members_sender.py
@@ -1,6 +1,6 @@
 import datetime
-from download_members_extractor import DownloadMembersExtractor
-from download_members_transformer import DownloadMembersTransformer
+from .download_members_extractor import DownloadMembersExtractor
+from .download_members_transformer import DownloadMembersTransformer
 from src.shared.environments import Environments
 from src.shared.infra.repositories import S3Manager
 
