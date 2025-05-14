184d65ffc7262c6d5cb55c8e1d6c8b8c085aba7c
LucasCrapino <163664488+LucasCrapino@users.noreply.github.com>
2025-05-14 09:56:55 -0300

    test: change get_all_actions_durations_by_user_id to original

--- src/shared/infra/repositories/action_repository_dynamo.py
+++ src/shared/infra/repositories/action_repository_dynamo.py
@@ -362,44 +362,29 @@ class ActionRepositoryDynamo(IActionRepository):
     
     def get_all_actions_durations_by_user_id(self, start_date: int , end_date: int) -> dict:
         expression = Attr('SK').begins_with('action#') & Attr('start_date').between(start_date, end_date) & Attr('end_date').lte(end_date)
-    
-        durations_by_user_id = {}
-        exclusive_start_key = {}
 
-        while exclusive_start_key is not None:
-            query_params = {
-                'KeyConditionExpression': expression,
-                'ExclusiveStartKey': exclusive_start_key,
-                'Select': 'ALL_ATTRIBUTES'
-            } 
-
-            if exclusive_start_key:
-                query_params['ExclusiveStartKey'] = exclusive_start_key
-
-            resp = self.dynamo.scan_items(**query_params)
+        resp = self.dynamo.scan_items(expression)
         
-            if resp.get("Count", 0) == 0:
-                return {}
+        if resp["Count"] == 0:
+            return {}
 
-            for item in resp['Items']:
-                action = ActionDynamoDTO.from_dynamo(item).to_entity()
-            
-                if action.duration is not None:
-                    if action.user_id in durations_by_user_id:
-                        durations_by_user_id[action.user_id] += action.duration
-                    else:
-                        durations_by_user_id[action.user_id] = action.duration
+        durations_by_user_id = {}
+        
+        for item in resp['Items']:
+            action = ActionDynamoDTO.from_dynamo(item).to_entity()
 
+            if action.duration is not None:
+                if action.user_id in durations_by_user_id:
+                    durations_by_user_id[action.user_id] += action.duration
+                else:
+                    durations_by_user_id[action.user_id] = action.duration
                 for associated_user_id in action.associated_members_user_ids:
                     if associated_user_id in durations_by_user_id:
                         durations_by_user_id[associated_user_id] += action.duration
                     else:
                         durations_by_user_id[associated_user_id] = action.duration
-
-            exclusive_start_key = resp.get("LastEvaluatedKey")
         
         return durations_by_user_id
-
 
     def get_action_durations_for_user(self, user_id: str, start_date: int, end_date: int) -> int:
         expression = Attr('SK').begins_with('action#') & Attr('start_date').between(start_date, end_date) & Attr('end_date').lte(end_date)
