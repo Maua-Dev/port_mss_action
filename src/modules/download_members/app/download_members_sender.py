c91944bd2fcf84d78a78f73f6b48414dc7023896
Lucas Crapino <lucascrapino@gmail.com>
2025-05-14 16:45:28 -0300

    fix: fix import

--- src/modules/download_members/app/download_members_sender.py
+++ src/modules/download_members/app/download_members_sender.py
@@ -2,7 +2,7 @@ import datetime
 from .download_members_extractor import DownloadMembersExtractor
 from .download_members_transformer import DownloadMembersTransformer
 from src.shared.environments import Environments
-from src.shared.infra.repositories import S3Manager
+from src.shared.infra.repositories.S3Manager import S3Manager
 
 
 repo = Environments.get_envs().get_member_repo()()
