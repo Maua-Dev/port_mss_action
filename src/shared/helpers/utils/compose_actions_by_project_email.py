8bb426b24b9304f17e2bfac49a4eed3b76028f3b
LucasCrapino <163664488+LucasCrapino@users.noreply.github.com>
2025-05-14 22:34:31 -0300

    feat: add email logic

--- src/shared/helpers/utils/compose_actions_by_project_email.py
+++ src/shared/helpers/utils/compose_actions_by_project_email.py
@@ -1,22 +1,51 @@
+from src.shared.domain.entities.member import Member
+from src.shared.domain.entities.project import Project
 
-def compose_actions_by_project_email(self) -> str:
-    message = f"""
+def compose_actions_by_project_email(self, member: Member, project: Project) -> str:
+    member_name = member.name
+    project_name = project.name
+    message = """
         <!DOCTYPE html>
-        <html lang="pt-br">
+        <html lang="pt-br" charset="UTF-8">
         <head>
-            <meta charset="UTF-8">
-            <title>Relatório de Ações po Projeto</title>
         </head>
-        <body style="font-family: Arial, sans-serif; margin: 0; padding: 20px; background-color: #f4f4f9;">
-            <div style="max-width: 600px; margin: auto; background-color: #ffffff; padding: 20px; border-radius: 8px; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);">
-                <h2 style="color: #333333;">Olá, Vitin!</h2>
-                <p style="color: #555555;">Segue arquivo com todas as ações de cada projeto da Dev Community Mauá.</p>
-                <a href="cid:relatorio_acoes.xlsx" style="display: inline-block; background-color: #4CAF50; color: #ffffff; padding: 10px 20px; text-decoration: none; border-radius: 4px;">Baixar Relatório</a>
-                <p style="color: #555555;">Para que você pare de perguntar: "E aí, grupo? Como estamos de projeto? (seguido de @ para cada membro de cada área)"</p>
-                <hr>
-                <p style="color: #555555;">Com carinho,<br>Equipe do Portal Interno (#ehobeckas😎)</p>
-            </div>
+        <body style="margin: 0; padding: 0; display: flex; align-items: center; justify-content: center; min-height: 100vh; background-color: white; font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">
+        <table class="main" style="width: 50vw; max-width: 600px; background-color: white; border-radius: 10px; box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.25); overflow: hidden;">
+            <tr>
+            <td>
+                <table class="TittleBox" style="width: 100%; background-color: #110e47; border-radius: 10px 10px 0 0;">
+                <tr>
+                    <td style="text-align: center; padding: 20px;">
+                    <img alt="PI Logo" src="https://d22wxe17x1tv7t.cloudfront.net/portalinterno.png" style="width: 60%;"/>
+                    <h1 style="color: #ffffff; margin-top: 10px;"><strong>Ação Invalidada!</strong></h1>
+                    </td>
+                </tr>
+                </table>
+                <table class="ContentBox" style="width: 100%; background-color: #282470;  border-top: 1px solid #ffffff; border-radius: 0 0 10px 10px;">
+                <tr>
+                    <td style="text-align: center; padding: 20px;">
+                    <div class="TextsBox" style="word-wrap: break-word;">
+                        <h2 style="color: #ffffff;">Olá, {member_name}<p>Segue relatório com todas as ações do projeto: {project_name}.
+                            Para mais informações, por favor, entre em contato com a equipe de suporte.</p></h2>
+                    </div>
+                    </td>
+                </tr>
+                </table>
+                <table class="BottomBox" style="width: 100%; background-color: #110e47; border-top: 1px solid #ffffff; border-radius: 0 0 10px 10px;">
+                <tr>
+                    <td style="text-align: center; padding: 20px;">
+                    <div class="TextsBox" style="color: #ffffff; word-wrap: break-word;">
+                        <h2>Atenciosamente,</h2>
+                        <h2><strong>Equipe do Portal Interno</strong>&#127744;</h2>
+                    </div>
+                    </td>
+                </tr>
+                </table>
+            </td>
+            </tr>
+        </table>
         </body>
         </html>
         """
+    message = message.format(member_name=member_name, project_name=project_name)
     return message
