b5ebb6cab8588f6e5d51852e95c24649619ad2ee
LucasCrapino <163664488+LucasCrapino@users.noreply.github.com>
2025-05-26 09:40:28 -0300

    fix: fix email strong title

--- src/shared/helpers/utils/compose_actions_by_project_email.py
+++ src/shared/helpers/utils/compose_actions_by_project_email.py
@@ -17,7 +17,7 @@ def compose_actions_by_project_email( member: Member, project: Project) -> str:
                 <tr>
                     <td style="text-align: center; padding: 20px;">
                     <img alt="PI Logo" src="https://d22wxe17x1tv7t.cloudfront.net/portalinterno.png" style="width: 60%;"/>
-                    <h1 style="color: #ffffff; margin-top: 10px;"><strong>Ação Invalidada!</strong></h1>
+                    <h1 style="color: #ffffff; margin-top: 10px;"><strong>Envio de ações do projeto</strong></h1>
                     </td>
                 </tr>
                 </table>
