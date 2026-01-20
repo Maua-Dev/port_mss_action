87b1f7e65a96a9cc60130ac4e6ebdcf25ab01942
Guilherme Guerreiro de Oliveira <155542954+GuiGuerreiroo@users.noreply.github.com>
2026-01-19 22:01:57 -0300

    fix: trying to fix the get_active_heads_and_directors

--- src/shared/infra/repositories/member_repository_dynamo.py
+++ src/shared/infra/repositories/member_repository_dynamo.py
@@ -87,7 +87,7 @@ class MemberRepositoryDynamo(IMemberRepository):
     
     def get_active_heads_and_directors(self) -> Optional[List[Member]]:
 
-        filter= Attr('ACTIVE').eq("ACTIVE") & Attr('ROLE').is_in([ROLE.HEAD.value, ROLE.DIRECTOR.value])
+        filter= Attr('active').eq("ACTIVE") & Attr('role').is_in([ROLE.HEAD.value, ROLE.DIRECTOR.value])
 
         response = self.dynamo.scan_items(
             filter_expression=filter
